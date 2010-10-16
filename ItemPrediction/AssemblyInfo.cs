7b2aa87c1d97d2796020e857f480f7845324f29a
Zeno Gantner <zeno.gantner@gmail.com>
2010-10-16 11:40:04 +0200

    filled in AssemblyInfos.cs

--- ItemPrediction/AssemblyInfo.cs
+++ ItemPrediction/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("ItemPrediction")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MyMediaLite item prediction")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("University of Hildesheim")]
+[assembly: AssemblyProduct("MyMediaLite")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.4.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
