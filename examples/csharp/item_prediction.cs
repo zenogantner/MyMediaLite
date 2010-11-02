fa5a1d9909ab826370d42dccc8e92680d627b6ab
Zeno Gantner <zeno.gantner@gmail.com>
2010-11-02 16:37:21 +0100

    add C# examples

---
+++ examples/csharp/item_prediction.cs
@@ -0,0 +1,34 @@
+using System;
+using MyMediaLite;
+using MyMediaLite.data;
+using MyMediaLite.eval;
+using MyMediaLite.io;
+using MyMediaLite.item_recommender;
+
+namespace ItemPrediction
+{
+	public class ItemPrediction
+	{
+		public static void Main(string[] args)
+		{
+			// load the data
+			var user_mapping = new EntityMapping();
+			var item_mapping = new EntityMapping();
+			var training_data = ItemRecommenderData.Read(args[0], user_mapping, item_mapping);
+			var relevant_items = item_mapping.InternalIDs;
+			var test_data = ItemRecommenderData.Read(args[1], user_mapping, item_mapping);
+
+			// set up the recommender
+			var recommender = new MostPopular();
+			recommender.SetCollaborativeData(training_data.First, training_data.Second);
+			recommender.Train();
+
+			// measure the accuracy on the test data set
+			var results = ItemPredictionEval.EvaluateItemRecommender(recommender, test_data.First, training_data.First, relevant_items);
+			Console.WriteLine("AUC={0}", results["AUC"]);
+
+			// make a prediction for a certain user and item
+			Console.WriteLine(recommender.Predict(user_mapping.ToInternalID(1), item_mapping.ToInternalID(1)));
+		}
+	}
+}
---
+++ examples/csharp/rating_prediction.cs
@@ -0,0 +1,38 @@
+using System;
+using MyMediaLite;
+using MyMediaLite.data;
+using MyMediaLite.eval;
+using MyMediaLite.io;
+using MyMediaLite.rating_predictor;
+
+namespace RatingPrediction
+{
+	public class RatingPrediction
+	{
+		public static void Main(string[] args)
+		{
+			double min_rating = 1;
+			double max_rating = 5;
+			
+			// load the data
+			var user_mapping = new EntityMapping();
+			var item_mapping = new EntityMapping();
+			var training_data = RatingPredictionData.Read(args[0], min_rating, max_rating, user_mapping, item_mapping);
+			var test_data = RatingPredictionData.Read(args[1], min_rating, max_rating, user_mapping, item_mapping);
+
+			// set up the recommender
+			var recommender = new UserItemBaseline();
+			recommender.MinRating = min_rating;
+			recommender.MaxRating = max_rating;
+			recommender.Ratings = training_data;
+			recommender.Train();
+
+			// measure the accuracy on the test data set
+			var results = RatingEval.EvaluateRated(recommender, test_data);
+			Console.WriteLine("RMSE={0} MAE={1}", results["RMSE"], results["MAE"]);
+
+			// make a prediction for a certain user and item
+			Console.WriteLine(recommender.Predict(user_mapping.ToInternalID(1), item_mapping.ToInternalID(1)));
+		}
+	}
+}
