fbb88fe7beca0269e044c0151792ef0ac481f1f2
Zeno Gantner <zeno.gantner@gmail.com>
2010-11-04 19:05:46 +0100

    add IAttributeToFactors interface

---
+++ MyMediaLite/experimental/attr_to_feature/IAttributeToFactors.cs
@@ -0,0 +1,36 @@
+// // Copyright (C) 2010 Zeno Gantner
+// //
+// // This file is part of MyMediaLite.
+// //
+// // MyMediaLite is free software: you can redistribute it and/or modify
+// // it under the terms of the GNU General Public License as published by
+// // the Free Software Foundation, either version 3 of the License, or
+// // (at your option) any later version.
+// //
+// // MyMediaLite is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// // GNU General Public License for more details.
+// //
+// //  You should have received a copy of the GNU General Public License
+// //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
+using System;
+using MyMediaLite.data_type;
+
+
+namespace MyMediaLite.experimental.attr_to_feature
+{
+	/// <summary>Interface for attribute-to-factor mappings</summary>
+	/// <remarks>
+	///
+	/// </remarks>
+	public interface IAttributeToFactors
+	{
+		/// <summary>Learn mapping from an attribute space to a latent feature space</summary>
+		void LearnAttributeToFactorMapping(SparseBooleanMatrix binary_attributes, Matrix<double> latent_features);
+	}
+}
+
