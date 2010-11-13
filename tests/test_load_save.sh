49fcd90d59b3295053daa926f6ef533b6060780e
Zeno Gantner <zeno.gantner@gmail.com>
2010-11-13 15:00:26 +0100

    add additional tests

---
+++ tests/test_load_save.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+# don't expect this to work, this is (currently) for internal testing purposes
+
+PROGRAM="mono --debug RatingPrediction.exe"
+
+echo "This will take about 5 minutes"
+
+cd src/RatingPrediction/bin/Debug
+
+for method in user-item-baseline matrix-factorization biased-matrix-factorization user-average item-average
+do
+     echo $PROGRAM u1.base u1.test $method save_model=tmp.model
+          $PROGRAM u1.base u1.test $method save_model=tmp.model | perl -pe "s/\w+ing_time \S+ ?//g" > output1.txt
+     echo $PROGRAM u1.base u1.test $method load_model=tmp.model
+          $PROGRAM u1.base u1.test $method load_model=tmp.model | perl -pe "s/\w+ing_time \S+ ?//g" > output2.txt
+     diff output1.txt output2.txt
+done
+
+for method in user-kNN-pearson user-kNN-cosine item-kNN-pearson item-kNN-cosine
+do
+     echo $PROGRAM u1.base u1.test $method k=20 save_model=tmp.model
+	  $PROGRAM u1.base u1.test $method k=20 save_model=tmp.model | perl -pe "s/\w+ing_time \S+ ?//g" > output1.txt
+     echo $PROGRAM u1.base u1.test $method k=20 load_model=tmp.model
+	  $PROGRAM u1.base u1.test $method k=20 load_model=tmp.model | perl -pe "s/\w+ing_time \S+ ?//g" > output2.txt
+     diff output1.txt output2.txt
+done
+
+rm tmp.model output1.txt output2.txt
