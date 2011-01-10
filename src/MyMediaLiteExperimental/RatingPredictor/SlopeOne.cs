3be59167e6a3a26fbc5070ec2156039acaf768f4
Zeno Gantner <zeno.gantner@gmail.com>
2011-01-10 12:08:51 +0100

    Slope-One improvements

--- src/MyMediaLiteExperimental/RatingPredictor/SlopeOne.cs
+++ src/MyMediaLiteExperimental/RatingPredictor/SlopeOne.cs
@@ -1,22 +1,23 @@
 // Copyright (C) 2011 Zeno Gantner
-// 
+//
 // This file is part of MyMediaLite.
-// 
+//
 // MyMediaLite is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // MyMediaLite is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU General Public License
 //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using MyMediaLite.Data;
 
@@ -31,53 +32,57 @@ namespace MyMediaLite.RatingPredictor
 	/// </remarks>
 	public class SlopeOne : Memory
 	{
-		// TODO implement CanPredict and ToString
-		
-		// TODO use an array here (we assume our item IDs to be dense ...
-  		private Dictionary<int, Dictionary<int, double>> diff_matrix;
-  		private Dictionary<int, Dictionary<int, int>> freq_matrix;
-		
+		// TODO use SparseMatrix<double> and SparseMatrix<int>
+  		private Dictionary<int, double>[] diff_matrix;
+  		private Dictionary<int, int>[] freq_matrix;
+
 		private double global_average;
-		
+
+		/// <inheritdoc/>
+		public override bool CanPredict(int user_id, int item_id)
+		{
+			if (user_id > MaxUserID)
+				return false;
+			if (item_id > MaxItemID)
+				return false;
+			if (freq_matrix[item_id].Count == 0)
+				return false;
+			return true;
+		}
+
+		/// <inheritdoc/>
 		public override double Predict(int user_id, int item_id)
 		{
-			var user_ratings = Ratings.ByUser[user_id];
-			
+			if (item_id > MaxItemID)
+				return global_average;
+
 			double prediction = 0.0;
 			int frequency = 0;
-			
-			// TODO make this faster
-		    foreach (int i in user_ratings.GetItems())
-       			try
+
+			foreach (RatingEvent r in Ratings.ByUser[user_id])
+			{
+				int f;
+				if (freq_matrix[item_id].TryGetValue(r.item_id, out f))
 				{
-       				double newval = ( diff_matrix[item_id][i] + user_ratings.FindRating(user_id, i).rating ) * freq_matrix[item_id][i];
-       				prediction += newval;
-       				frequency += freq_matrix[item_id][i];
-       			} catch (KeyNotFoundException) {}	
+					prediction += ( diff_matrix[item_id][r.item_id] + r.rating ) * f;
+					frequency += f;
+				}
+			}
 
 			if (frequency == 0)
 				return global_average;
-			
+
     		return (double) prediction / frequency;
 		}
-		
+
+		/// <inheritdoc/>
 		public override void Train()
 		{
-			global_average = Ratings.Average;
-			
-			// create data structure
-    		diff_matrix = new Dictionary<int ,Dictionary<int, double>>();
-    		freq_matrix = new Dictionary<int ,Dictionary<int, int>>();
-    
+			InitModel();
+
+			// compute difference sums and frequencies
 			foreach (var user_ratings in Ratings.ByUser)
-			{
 				foreach (RatingEvent r in user_ratings)
-				{
-					if (! diff_matrix.ContainsKey(r.item_id))					    
-					{
-	          			diff_matrix[r.item_id] = new Dictionary<int, double>();
-	          			freq_matrix[r.item_id] = new Dictionary<int, int>();
-	        		}
 	        		foreach (RatingEvent r2 in user_ratings)
 					{
 	          			if (!freq_matrix[r.item_id].ContainsKey(r2.item_id))
@@ -85,30 +90,52 @@ namespace MyMediaLite.RatingPredictor
 							freq_matrix[r.item_id][r2.item_id] = 0;
 	          				diff_matrix[r.item_id][r2.item_id] = 0.0;
 						}
-							
+
 	          			freq_matrix[r.item_id][r2.item_id] += 1;
 	          			diff_matrix[r.item_id][r2.item_id] += r.rating - r2.rating;
 	        		}
-      			}
-    		}
-			var relevant_items = diff_matrix.Keys.ToList();
-			foreach (int i in relevant_items)
+
+			// compute average differences
+			for (int i = 0; i <= MaxItemID; i++)
 			{
-				var relevant_items2 = diff_matrix[i].Keys.ToList();
-				foreach (int j in relevant_items2)
+				//var relevant_items = diff_matrix[i].Keys.ToList();
+				foreach (int j in diff_matrix[i].Keys.ToList())
 					diff_matrix[i][j] /= freq_matrix[i][j];
 			}
 		}
 
+		private void InitModel()
+		{
+			// default value if no prediction can be made
+			global_average = Ratings.Average;
+
+			// create data structure
+    		diff_matrix = new Dictionary<int, double>[MaxItemID + 1];
+    		freq_matrix = new Dictionary<int, int>[MaxItemID + 1];
+    		for (int i = 0; i <= MaxItemID; i++)
+			{
+      			diff_matrix[i] = new Dictionary<int, double>();
+      			freq_matrix[i] = new Dictionary<int, int>();
+			}
+		}
+
+		/// <inheritdoc/>
 		public override void LoadModel(string file)
 		{
+			InitModel();
 			throw new NotImplementedException();
-		}		
-		
+		}
+
+		/// <inheritdoc/>
 		public override void SaveModel(string file)
 		{
 			throw new NotImplementedException();
 		}
-		                               
+
+		/// <inheritdoc/>
+		public override string ToString()
+		{
+			 return "slope-one";
+		}
 	}
 }
