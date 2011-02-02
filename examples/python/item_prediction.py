91e4db2d93f10ba4cc48485a3fe46461025061cc
Zeno Gantner <zeno.gantner@gmail.com>
2011-02-02 18:39:00 +0100

    shorter python examples

--- examples/python/item_prediction.py
+++ examples/python/item_prediction.py
@@ -2,22 +2,22 @@
 
 import clr
 clr.AddReference("MyMediaLite.dll")
-import MyMediaLite
+from MyMediaLite import *
 
 # load the data
-user_mapping = MyMediaLite.Data.EntityMapping()
-item_mapping = MyMediaLite.Data.EntityMapping()
-training_data = MyMediaLite.IO.ItemRecommenderData.Read("u1.base", user_mapping, item_mapping)
+user_mapping = Data.EntityMapping()
+item_mapping = Data.EntityMapping()
+training_data = IO.ItemRecommenderData.Read("u1.base", user_mapping, item_mapping)
 relevant_items = item_mapping.InternalIDs
-test_data = MyMediaLite.IO.ItemRecommenderData.Read("u1.test", user_mapping, item_mapping)
+test_data = IO.ItemRecommenderData.Read("u1.test", user_mapping, item_mapping)
 
 # set up the recommender
-recommender = MyMediaLite.ItemRecommendation.MostPopular()
+recommender = ItemRecommendation.MostPopular()
 recommender.SetCollaborativeData(training_data);
 recommender.Train()
 
 # measure the accuracy on the test data set
-print MyMediaLite.Eval.ItemPredictionEval.Evaluate(recommender, test_data, training_data, relevant_items)
+print Eval.ItemPredictionEval.Evaluate(recommender, test_data, training_data, relevant_items)
 
 # make a prediction for a certain user and item
 print recommender.Predict(user_mapping.ToInternalID(1), item_mapping.ToInternalID(1))
--- examples/python/rating_prediction.py
+++ examples/python/rating_prediction.py
@@ -2,26 +2,26 @@
 
 import clr
 clr.AddReference("MyMediaLite.dll")
-import MyMediaLite
+from MyMediaLite import *
 
 min_rating = 1
 max_rating = 5
 
 # load the data
-user_mapping = MyMediaLite.Data.EntityMapping()
-item_mapping = MyMediaLite.Data.EntityMapping()
-training_data = MyMediaLite.IO.RatingPredictionData.Read("u1.base", min_rating, max_rating, user_mapping, item_mapping)
-test_data = MyMediaLite.IO.RatingPredictionData.Read("u1.test", min_rating, max_rating, user_mapping, item_mapping)
+user_mapping = Data.EntityMapping()
+item_mapping = Data.EntityMapping()
+training_data = IO.RatingPredictionData.Read("u1.base", min_rating, max_rating, user_mapping, item_mapping)
+test_data = IO.RatingPredictionData.Read("u1.test", min_rating, max_rating, user_mapping, item_mapping)
 
 # set up the recommender
-recommender = MyMediaLite.RatingPrediction.UserItemBaseline()
+recommender = RatingPrediction.UserItemBaseline()
 recommender.MinRating = min_rating
 recommender.MaxRating = max_rating
 recommender.Ratings = training_data
 recommender.Train()
 
 # measure the accuracy on the test data set
-print MyMediaLite.Eval.RatingEval.Evaluate(recommender, test_data)
+print Eval.RatingEval.Evaluate(recommender, test_data)
 
 # make a prediction for a certain user and item
 print recommender.Predict(user_mapping.ToInternalID(1), item_mapping.ToInternalID(1))
