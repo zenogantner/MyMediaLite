021cbf490dd4962e4c7d64743e49e523a6138da3
Zeno Gantner <zeno.gantner@gmail.com>
2011-02-06 22:47:43 +0100

    add memory usage info class

---
+++ src/MyMediaLite/Util/Memory.cs
@@ -0,0 +1,31 @@
+// Copyright (C) 2011 Zeno Gantner
+// 
+// This file is part of MyMediaLite.
+// 
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Diagnostics;
+
+namespace MyMediaLite.Util
+{
+	public class Memory
+	{
+		public static void ReportUsage()
+		{
+			Console.Error.WriteLine("memory {0}", GC.GetTotalMemory(true) / (1024*1024));
+		}
+	}
+}
+
