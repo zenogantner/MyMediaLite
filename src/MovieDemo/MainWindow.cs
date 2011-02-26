9b7fdc84ef40931f5277dc70be3a2d9e775cc064
Zeno Gantner <zeno.gantner@gmail.com>
2011-02-26 13:24:45 +0100

    GUI Demo: complete ordering features (for now)

--- src/MovieDemo/MainWindow.cs
+++ src/MovieDemo/MainWindow.cs
@@ -31,10 +31,12 @@ public partial class MainWindow : Window
 
 	TreeModelFilter filter;
 	TreeModelSort sorter;
+	TreeViewColumn prediction_column = new TreeViewColumn();
+	TreeViewColumn rating_column = new TreeViewColumn();
 	TreeViewColumn movie_column = new TreeViewColumn();
 
 	Gdk.Color white = new Gdk.Color(0xff, 0xff, 0xff);
-	
+
 	RatingData training_data;
 
 	//RatingPredictor rating_predictor = new BiasedMatrixFactorization();
@@ -52,11 +54,11 @@ public partial class MainWindow : Window
 
 	double min_rating = 1;
 	double max_rating = 5;
-	
+
 	public MainWindow() : base( WindowType.Toplevel)
 	{
 		ni.NumberDecimalDigits = '.'; // ensure correct comma separator (for English)
-		
+
 		// TODO integrate internal IDs - load after training data ...
 		Console.Error.Write("Reading in movie data ... ");
 		//movies.Read("/home/mrg/data/ml10m/movies.dat"); // TODO param
@@ -79,7 +81,7 @@ public partial class MainWindow : Window
 
 		current_user_id = rating_predictor.MaxUserID;
 		rating_predictor.AddUser(current_user_id);
-		
+
 		PredictAllRatings();
 
 		// build main window
@@ -95,23 +97,25 @@ public partial class MainWindow : Window
 		filter_entry.Changed += OnFilterEntryTextChanged;
 
 		// create a column for the prediction
-		TreeViewColumn prediction_column = new TreeViewColumn();
 		prediction_column.Title = "Prediction";
 		CellRendererText prediction_cell = new CellRendererText();
 		prediction_cell.BackgroundGdk = white;
 		prediction_column.PackStart(prediction_cell, true);
-		//prediction_column.SortIndicator = true;
+		prediction_column.SortIndicator = true;
+		prediction_column.Clickable = true;
+		prediction_column.Clicked += new EventHandler( PredictionColumnClicked );
 
 		// create a column for the rating
-		TreeViewColumn rating_column = new TreeViewColumn();
 		rating_column.Title = "Rating";
 		CellRendererText rating_cell = new CellRendererText();
 		rating_cell.Editable = true;
 		rating_cell.Edited += RatingCellEdited;
 		rating_cell.BackgroundGdk = white;
 		rating_column.PackStart(rating_cell, true);
-		//rating_column.SortIndicator = true;
-
+		rating_column.SortIndicator = true;
+		rating_column.Clickable = true;
+		rating_column.Clicked += new EventHandler( RatingColumnClicked );
+		
 		// set up a column for the movie title
 		movie_column.Title = "Movie";
 		CellRendererText movie_cell = new CellRendererText();
@@ -130,8 +134,8 @@ public partial class MainWindow : Window
 		rating_column.SetCellDataFunc(rating_cell, new TreeCellDataFunc(RenderRating));
 		movie_column.SetCellDataFunc(movie_cell, new TreeCellDataFunc(RenderMovieTitle));
 
-		var movie_store = new ListStore( typeof(Movie) );		
-		
+		var movie_store = new ListStore( typeof(Movie) );
+
 		// Add some data to the store
 		foreach (Movie movie in movies.movie_list)
 			movie_store.AppendValues( movie );
@@ -139,10 +143,10 @@ public partial class MainWindow : Window
 		filter = new TreeModelFilter(movie_store, null);
 		// specify the function that determines which rows to filter out and which ones to display
 		filter.VisibleFunc = new TreeModelFilterVisibleFunc(FilterTree);
-		
+
 		sorter = new TreeModelSort(filter);
-		sorter.DefaultSortFunc = CompareTitle;
-			
+		sorter.DefaultSortFunc = ComparePredictionReversed;
+
 		treeview1.Model = sorter;
 		treeview1.ShowAll();
 	}
@@ -152,7 +156,7 @@ public partial class MainWindow : Window
 		if (movie_column.SortOrder == SortType.Ascending)
 		{
 			movie_column.SortOrder = SortType.Descending;
-			sorter.DefaultSortFunc = CompareTitleReverse;
+			sorter.DefaultSortFunc = CompareTitleReversed;
 		}
 		else
 		{
@@ -161,34 +165,68 @@ public partial class MainWindow : Window
 		}
 	}
 
- 	private int CompareTitleReverse(TreeModel model, TreeIter a, TreeIter b)
+	// TODO have only one sorting function
+ 	private int CompareTitleReversed(TreeModel model, TreeIter a, TreeIter b)
 	{
 		Movie movie1 = (Movie) model.GetValue(a, 0);
 		Movie movie2 = (Movie) model.GetValue(b, 0);
-		
+
 		return string.Compare(movie2.Title, movie1.Title);
-	}	
-	
+	}
+
  	private int CompareTitle(TreeModel model, TreeIter a, TreeIter b)
 	{
 		Movie movie1 = (Movie) model.GetValue(a, 0);
 		Movie movie2 = (Movie) model.GetValue(b, 0);
-		
+
 		return string.Compare(movie1.Title, movie2.Title);
+	}
+
+	private void PredictionColumnClicked(object o, EventArgs args)
+	{
+		if (prediction_column.SortOrder == SortType.Ascending)
+		{
+			prediction_column.SortOrder = SortType.Descending;
+			sorter.DefaultSortFunc = ComparePrediction;
+		}
+		else
+		{
+			prediction_column.SortOrder = SortType.Ascending;
+			sorter.DefaultSortFunc = ComparePredictionReversed;
+		}
+	}
+
+ 	private int ComparePredictionReversed(TreeModel model, TreeIter a, TreeIter b)
+	{
+		Movie movie1 = (Movie) model.GetValue(a, 0);
+		Movie movie2 = (Movie) model.GetValue(b, 0);
+
+		double prediction1 = -1;
+		predictions.TryGetValue(movie1.ID, out prediction1);
+		double prediction2 = -1;
+		predictions.TryGetValue(movie2.ID, out prediction2);
+
+		double diff = prediction2 - prediction1;
+
+		if (diff > 0)
+			return 1;
+		if (diff < 0)
+			return -1;
+		return 0;
 	}
 
  	private int ComparePrediction(TreeModel model, TreeIter a, TreeIter b)
 	{
 		Movie movie1 = (Movie) model.GetValue(a, 0);
 		Movie movie2 = (Movie) model.GetValue(b, 0);
-		
+
 		double prediction1 = -1;
 		predictions.TryGetValue(movie1.ID, out prediction1);
 		double prediction2 = -1;
 		predictions.TryGetValue(movie2.ID, out prediction2);
-		
+
 		double diff = prediction1 - prediction2;
-	
+
 		if (diff > 0)
 			return 1;
 		if (diff < 0)
@@ -196,18 +234,32 @@ public partial class MainWindow : Window
 		return 0;
 	}
 
- 	private int CompareRating(TreeModel model, TreeIter a, TreeIter b)
+	private void RatingColumnClicked(object o, EventArgs args)
+	{
+		if (rating_column.SortOrder == SortType.Ascending)
+		{
+			rating_column.SortOrder = SortType.Descending;
+			sorter.DefaultSortFunc = CompareRating;
+		}
+		else
+		{
+			rating_column.SortOrder = SortType.Ascending;
+			sorter.DefaultSortFunc = CompareRatingReversed;
+		}
+	}	
+	
+ 	private int CompareRatingReversed(TreeModel model, TreeIter a, TreeIter b)
 	{
 		Movie movie1 = (Movie) model.GetValue(a, 0);
 		Movie movie2 = (Movie) model.GetValue(b, 0);
 
-		double rating1 = -1;
+		double rating1;
 		ratings.TryGetValue(movie1.ID, out rating1);
-		double rating2 = -1;
-		ratings.TryGetValue(movie2.ID, out rating2);		
-		
-		double diff = rating1 - rating2;
-		
+		double rating2;
+		ratings.TryGetValue(movie2.ID, out rating2);
+
+		double diff = rating2 - rating1;
+
 		if (diff > 0)
 			return 1;
 		if (diff < 0)
@@ -215,11 +267,30 @@ public partial class MainWindow : Window
 		return 0;
 	}	
 	
+ 	private int CompareRating(TreeModel model, TreeIter a, TreeIter b)
+	{
+		Movie movie1 = (Movie) model.GetValue(a, 0);
+		Movie movie2 = (Movie) model.GetValue(b, 0);
+
+		double rating1;
+		ratings.TryGetValue(movie1.ID, out rating1);
+		double rating2;
+		ratings.TryGetValue(movie2.ID, out rating2);
+
+		double diff = rating1 - rating2;
+
+		if (diff > 0)
+			return 1;
+		if (diff < 0)
+			return -1;
+		return 0;
+	}
+
 	private void RatingCellEdited(object o, EditedArgs args)
 	{
 		TreeIter iter;
 		treeview1.Model.GetIter(out iter, new TreePath(args.Path));
-	 
+
 		Movie movie = (Movie) treeview1.Model.GetValue(iter, 0);
 		string input = args.NewText;
 		try
@@ -230,34 +301,34 @@ public partial class MainWindow : Window
 				rating = max_rating;
 			if (rating < min_rating)
 				rating = min_rating;
-			
+
 			ratings[movie.ID] = rating;
 			rating_predictor.AddRating(current_user_id, item_mapping.ToInternalID(movie.ID), rating);
-			
+
 			PredictAllRatings();
 		}
 		catch (FormatException)
 		{
 			Console.Error.WriteLine("Could not parse input '{0}' as a number.", input);
-		}		
-	}	
-	
+		}
+	}
+
 	private void RenderPrediction(TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
 	{
 		Movie movie = (Movie) model.GetValue(iter, 0);
-		
+
 		double prediction = -1;
 		predictions.TryGetValue(movie.ID, out prediction);
-		
+
 		(cell as CellRendererText).Text = string.Format(ni, "{0,0:0.#}", prediction);
 	}
 
 	private void RenderRating(TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
 	{
 		Movie movie = (Movie) model.GetValue(iter, 0);
-		
-		double rating = -1;	
-		
+
+		double rating = -1;
+
 		if (ratings.TryGetValue(movie.ID, out rating))
 			(cell as CellRendererText).Text = string.Format(ni, "{0}", rating);
 		else
@@ -297,7 +368,7 @@ public partial class MainWindow : Window
 		// compute ratings
 		for (int i = 0; i <= rating_predictor.MaxItemID; i++)
 			predictions[item_mapping.ToOriginalID(i)] = rating_predictor.Predict(current_user_id, i);
-			
+
 		Console.Error.WriteLine("done.");
 	}
 
