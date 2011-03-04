b0a4e5ed9c44ded47b55408aab15d7e5dcd9ec01
Zeno Gantner <zeno.gantner@gmail.com>
2011-03-04 19:04:11 +0100

    add script for movie demo models

---
+++ scripts/prepare_models_for_moviedemo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# to be run from the root directory of MyMediaLite
+
+# ML1M
+time mono --debug src/RatingPrediction/bin/Debug/RatingPrediction.exe data/ml1m/ratings.txt data/ml1m/ratings.txt BiasedMatrixFactorization bias_reg=0.001 reg=0.045 num_iter=60 save_model=src/MovieDemo/ml1m-bmf.model min_rating=1
+
+# ML10M
+time mono --debug src/RatingPrediction/bin/Debug/RatingPrediction.exe data/ml10m/ratings.txt data/ml10m/ratings.txt BiasedMatrixFactorization bias_reg=0.001 reg=0.045 num_iter=60 save_model=src/MovieDemo/ml10m-bmf.model min_rating=0.5
+# TODO find better hyperparameters
