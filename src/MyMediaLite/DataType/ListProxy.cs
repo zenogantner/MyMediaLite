0538d9ad3a9dce4ba7e56833690bdb2053ffab7c
Zeno Gantner <zeno.gantner@gmail.com>
2011-03-11 18:52:11 +0100

    add ListProxy data type and RatingsTest unit test

---
+++ src/MyMediaLite/DataType/ListProxy.cs
@@ -0,0 +1,69 @@
+// Copyright (C) 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyMediaLite.DataType
+{
+	public class ListProxy<T> : IList<T>
+	{
+		IList<T> list;
+		IList<int> indices;
+
+		public ListProxy(IList<T> list, IList<int> indices)
+		{
+			this.list = list;
+			this.indices = indices;
+		}
+
+		public T this[int index]
+		{
+			get {
+				return list[indices[index]];
+			}
+			set {
+				list[indices[index]] = value;
+			}
+		}
+
+		public int Count { get { return list.Count; } }
+
+		public bool IsReadOnly { get { return true; } }
+
+
+		public void Add(T item) { throw new NotSupportedException(); }
+
+		public void Clear() { throw new NotSupportedException(); }
+
+		public bool Contains(T item) { return list.Contains(item); }
+
+		public void CopyTo(T[] array, int i) { throw new NotImplementedException(); }
+
+		public void Insert(int index, T item) { throw new NotSupportedException(); }
+
+		public int IndexOf(T item) { throw new NotSupportedException(); }
+
+		public bool Remove(T item) { throw new NotSupportedException(); }
+
+		public void RemoveAt(int index) { throw new NotSupportedException(); }
+
+		IEnumerator IEnumerable.GetEnumerator() { return list.GetEnumerator(); }
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator() { return list.GetEnumerator(); }
+	}
+}
---
+++ src/MyMediaLiteTest/Data/RatingsTest.cs
@@ -0,0 +1,137 @@
+// Copyright (C) 2010 Christina Lichtenthäler
+// Copyright (C) 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyMediaLite.Data;
+using NUnit.Framework;
+
+namespace MyMediaLiteTest
+{
+	[TestFixture()]
+	public class RatingDataTest
+	{
+		[Test()] public void TestMaxUserIDItemID()
+		{
+			var ratings = new Ratings();
+			ratings.Add(1, 4, 0.3);
+			ratings.Add(1, 8, 0.2);
+			ratings.Add(2, 4, 0.2);
+			ratings.Add(2, 2, 0.6);
+			ratings.Add(2, 5, 0.4);
+			ratings.Add(3, 7, 0.2);
+			ratings.Add(6, 3, 0.3);
+
+			Assert.AreEqual(6, ratings.MaxUserID);
+			Assert.AreEqual(8, ratings.MaxItemID);
+		}
+
+		[Test()] public void TestAddRating()
+		{
+			var ratings = new Ratings();
+			ratings.Add(1, 4, 0.3);
+			ratings.Add(1, 8, 0.2);
+			ratings.Add(2, 4, 0.2);
+			ratings.Add(2, 2, 0.6);
+			ratings.Add(2, 5, 0.4);
+			ratings.Add(3, 7, 0.2);
+			ratings.Add(6, 3, 0.3);
+
+			Assert.AreEqual(0.4, ratings.Get(2, 5));
+			Assert.AreEqual(0.3, ratings.Get(1, 4));
+			Assert.AreEqual(0.3, ratings.Get(6, 3));
+		}
+
+		[Test()] public void TestRemoveAt()
+		{
+			var ratings = new Ratings();
+			ratings.Add(1, 4, 0.3);
+			ratings.Add(1, 8, 0.2);
+			ratings.Add(2, 4, 0.2);
+			ratings.Add(2, 2, 0.6);
+			ratings.Add(2, 5, 0.4);
+			ratings.Add(3, 7, 0.2);
+			ratings.Add(3, 3, 0.3);
+			ratings.Add(6, 3, 0.3);
+
+			Assert.AreEqual(0.4, ratings.Get(2, 5));
+			ratings.RemoveAt(ratings.GetIndex(2, 5));
+			ratings.RemoveAt(ratings.GetIndex(6, 3));
+			
+			double r;
+			Assert.IsFalse(ratings.TryGet(2, 5, out r));
+		}
+
+		[Test()] public void TestRemoveUser()
+		{
+			var ratings = new Ratings();
+			ratings.Add(1, 4, 0.3);
+			ratings.Add(1, 8, 0.2);
+			ratings.Add(2, 4, 0.2);
+			ratings.Add(2, 2, 0.6);
+			ratings.Add(2, 5, 0.4);
+			ratings.Add(3, 7, 0.2);
+			ratings.Add(3, 3, 0.3);
+
+			Assert.AreEqual(0.4, ratings.Get(2, 5));
+			ratings.RemoveUser(2);
+			Assert.IsNull(ratings.Get(2, 5));
+		}
+
+		[Test()] public void TestRemoveItem()
+		{
+			var ratings = new Ratings();
+			ratings.Add(1, 4, 0.3);
+			ratings.Add(1, 8, 0.2);
+			ratings.Add(2, 4, 0.2);
+			ratings.Add(2, 2, 0.6);
+			ratings.Add(2, 5, 0.4);
+			ratings.Add(3, 4, 0.2);
+			ratings.Add(3, 3, 0.3);
+
+			Assert.AreEqual(0.2, ratings.Get(2, 4));
+			ratings.RemoveItem(4);
+			double r;
+			Assert.IsFalse(ratings.TryGet(2, 4, out r));
+		}
+
+		[Test()] public void TestGet()
+		{
+			var ratings = new Ratings();
+			ratings.Add(1, 4, 0.3);
+			ratings.Add(1, 8, 0.2);
+			ratings.Add(2, 4, 0.2);
+			ratings.Add(2, 2, 0.6);
+			ratings.Add(2, 5, 0.4);
+			ratings.Add(3, 4, 0.2);
+			ratings.Add(3, 3, 0.3);
+			ratings.Add(6, 3, 0.3);
+
+			// test Get
+			Assert.AreEqual(0.2, ratings.Get(2, 4));
+			Assert.AreEqual(0.3, ratings.Get(3, 3));
+			Assert.AreEqual(0.3, ratings.Get(6, 3));
+			
+			// test index[,]
+			Assert.AreEqual(0.3, ratings[1, 4]);
+			Assert.AreEqual(0.2, ratings[1, 8]);
+			Assert.AreEqual(0.6, ratings[2, 2]);			
+		}
+	}
+}
