c0618ebef2a4294c62a5760be48e683c92fbab36
Zeno Gantner <zeno.gantner@gmail.com>
2011-03-11 21:05:03 +0100

    add RatingsProxy class

--- src/MyMediaLite/DataType/ListProxy.cs
+++ src/MyMediaLite/DataType/ListProxy.cs
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
