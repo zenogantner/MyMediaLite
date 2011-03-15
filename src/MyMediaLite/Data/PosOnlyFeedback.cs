2f850fcbcc6b1e49d0855b3ccb77e41c9781c785
Zeno Gantner <zeno.gantner@gmail.com>
2011-03-15 17:36:11 +0100

    create PosOnlyFeedback data structure that should save memory in some cases

--- src/MyMediaLite/Data/PosOnlyFeedback.cs
+++ src/MyMediaLite/Data/PosOnlyFeedback.cs
@@ -1,17 +1,17 @@
 // Copyright (C) 2011 Zeno Gantner
-// 
+//
 // This file is part of MyMediaLite.
-// 
+//
 // MyMediaLite is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // MyMediaLite is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,36 +21,48 @@ using MyMediaLite.DataType;
 
 namespace MyMediaLite.Data
 {
+	/// <summary>Data structure for implicit, positive-only user feedback</summary>
+	/// <remarks>
+	/// This data structure does currently NOT support online updates.
+	/// Adding this functionality is trivial, though.
+	/// </remarks>
 	public class PosOnlyFeedback
 	{
-		/*
-		public IList<IList<int>> ByUser { get; private set; }
-		
-		public SparseBooleanMatrix ByUserMatrix
+		/// <summary>By-user access, users are stored in the rows, items in the culumns</summary>
+		public SparseBooleanMatrix UserMatrix { get; private set; }
+
+		/// <summary>By-item access, items are stored in the rows, users in the culumns</summary>		
+		public SparseBooleanMatrix ItemMatrix
 		{
-			get;
+			get {
+				if (item_matrix == null)
+					BuildItemMatrix();
+
+				return item_matrix;
+			}
 		}
-		SparseBooleanMatrix by_user;
-		
-		public SparseBooleanMatrix ByItemMatrix
-		{
-			get;
-		}
-		SparseBooleanMatrix by_item;
-		
+		SparseBooleanMatrix item_matrix;
+
+		/// <summary>Create a PosOnlyFeedback object</summary>
 		public PosOnlyFeedback()
 		{
-			ByUser = new List<IList<int>>();
+			UserMatrix = new SparseBooleanMatrix();
 		}
-		
+
+		void BuildItemMatrix()
+		{
+			item_matrix = new SparseBooleanMatrix();
+			for (int i = 0; i < UserMatrix.NumberOfRows; i++)
+				foreach (int j in UserMatrix[i])
+					item_matrix[j, i] = true;
+		}
+
+		/// <summary>Add a user-item event to the data structure</summary>
+		/// <param name="user_id">the user ID</param>
+		/// <param name="item_id">the item ID</param>
 		public void Add(int user_id, int item_id)
 		{
-			while (user_id >= ByUser.Count)
-				ByUser.Add(new List<int>());
-			
-			ByUser[user_id].Add(item_id);
+			UserMatrix[user_id, item_id] = true;
 		}
-		*/
 	}
 }
-
