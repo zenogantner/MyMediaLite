b77473a98472bf9ca5024327a9ef0c245cb295cc
Zeno Gantner <zeno.gantner@gmail.com>
2011-03-15 15:34:32 +0100

    rename KDD Cup class

---
+++ src/MyMediaLite/Data/PosOnlyFeedback.cs
@@ -0,0 +1,56 @@
+// Copyright (C) 2011 Zeno Gantner
+// 
+// This file is part of MyMediaLite.
+// 
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using MyMediaLite.DataType;
+
+namespace MyMediaLite.Data
+{
+	public class PosOnlyFeedback
+	{
+		/*
+		public IList<IList<int>> ByUser { get; private set; }
+		
+		public SparseBooleanMatrix ByUserMatrix
+		{
+			get;
+		}
+		SparseBooleanMatrix by_user;
+		
+		public SparseBooleanMatrix ByItemMatrix
+		{
+			get;
+		}
+		SparseBooleanMatrix by_item;
+		
+		public PosOnlyFeedback()
+		{
+			ByUser = new List<IList<int>>();
+		}
+		
+		public void Add(int user_id, int item_id)
+		{
+			while (user_id >= ByUser.Count)
+				ByUser.Add(new List<int>());
+			
+			ByUser[user_id].Add(item_id);
+		}
+		*/
+	}
+}
+
