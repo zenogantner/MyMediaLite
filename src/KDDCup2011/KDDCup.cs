25c89746f796dcf083ded4021f0385b7da2d8302
Zeno Gantner <zeno.gantner@gmail.com>
2011-03-16 15:38:51 +0100

    add C# program for KDD Cup 2011 - only supports track 1 so far

---
+++ src/KDDCup2011/KDDCup.cs
@@ -0,0 +1,377 @@
+// Copyright (C) 2010, 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using MyMediaLite;
+using MyMediaLite.Data;
+using MyMediaLite.DataType;
+using MyMediaLite.Eval;
+using MyMediaLite.IO;
+using MyMediaLite.IO.KDDCup2011;
+using MyMediaLite.RatingPrediction;
+using MyMediaLite.Util;
+
+/// <summary>Rating prediction program, see Usage() method for more information</summary>
+public static class RatingPrediction
+{
+	static NumberFormatInfo ni = new NumberFormatInfo();
+
+	// data sets
+	static IRatings training_data;
+	static IRatings validation_data;
+	static IRatings track1_test_data;
+	static Dictionary<int, IList<int>> track2_test_data;
+	static KDDCupItems item_relations;
+
+	// recommenders
+	static RatingPredictor recommender = null;
+
+	// time statistics
+	static List<double> training_time_stats = new List<double>();
+	static List<double> fit_time_stats      = new List<double>();
+	static List<double> eval_time_stats     = new List<double>();
+	static List<double> rmse_eval_stats     = new List<double>();
+
+	// global command line parameters
+	static bool compute_fit;
+	static string save_model_file;
+	static string load_model_file;	
+	static int max_iter;
+	static int find_iter;
+	static int cross_validation;
+	static double epsilon;
+	static double rmse_cutoff;
+	static double mae_cutoff;
+	static bool no_eval;
+	static string prediction_file; // TODO do something useful with this
+	
+
+	static void Usage(string message)
+	{
+		Console.WriteLine(message);
+		Console.WriteLine();
+		Usage(-1);
+	}
+
+	static void Usage(int exit_code)
+	{
+		Console.WriteLine(@"
+MyMediaLite KDD Cup 2011 tool
+
+ usage:  RatingPrediction.exe TRACK_NO METHOD [ARGUMENTS] [OPTIONS]
+
+  use '-' for either TRAINING_FILE or TEST_FILE to read the data from STDIN
+
+  methods (plus arguments and their defaults):");
+
+			Console.Write("   - ");
+			Console.WriteLine(string.Join("\n   - ", Recommender.List("MyMediaLite.RatingPrediction")));
+
+			Console.WriteLine(@"method ARGUMENTS have the form name=value
+
+  general OPTIONS have the form name=value
+   - option_file=FILE           read options from FILE (line format KEY: VALUE)
+   - random_seed=N              set random seed to N
+   - data_dir=DIR               load all files from DIR
+   - save_model=FILE            save computed model to FILE
+   - load_model=FILE            load model from FILE
+   - no_eval=BOOL               do not evaluate
+   - prediction_file=FILE       write the predictions to  FILE ('-' for STDOUT)
+   - cross_validation=K         perform k-fold crossvalidation on the training data
+                                 (ignores the test data)
+
+  options for finding the right number of iterations (MF methods)
+   - find_iter=N                give out statistics every N iterations
+   - max_iter=N                 perform at most N iterations
+   - epsilon=NUM                abort iterations if RMSE is more than best result plus NUM
+   - rmse_cutoff=NUM            abort if RMSE is above NUM
+   - mae_cutoff=NUM             abort if MAE is above NUM
+   - compute_fit=BOOL           display fit on training data every find_iter iterations");
+
+		Environment.Exit(exit_code);
+	}
+
+    static void Main(string[] args)
+    {
+		Assembly assembly = Assembly.GetExecutingAssembly();
+		Assembly.LoadFile(Path.GetDirectoryName(assembly.Location) + Path.DirectorySeparatorChar + "MyMediaLiteExperimental.dll");
+
+		double min_rating = 0;
+		double max_rating = 100;
+		
+		AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Handlers.UnhandledExceptionHandler);
+		Console.CancelKeyPress += new ConsoleCancelEventHandler(AbortHandler);
+		ni.NumberDecimalDigits = '.';
+
+		// check number of command line parameters
+		if (args.Length < 2)
+			Usage("Not enough arguments.");
+
+		// read command line parameters
+		uint track_no = uint.Parse(args[0]);
+		string method = args[1];
+
+		CommandLineParameters parameters = null;
+		try	{ parameters = new CommandLineParameters(args, 2); }
+		catch (ArgumentException e) { Usage(e.Message);	}
+
+		// arguments for iteration search
+		find_iter      = parameters.GetRemoveInt32(  "find_iter",   0);
+		max_iter       = parameters.GetRemoveInt32(  "max_iter",    500);
+		compute_fit        = parameters.GetRemoveBool(   "compute_fit", false);
+		epsilon     = parameters.GetRemoveDouble( "epsilon",     0);
+		rmse_cutoff = parameters.GetRemoveDouble( "rmse_cutoff", double.MaxValue);
+		mae_cutoff  = parameters.GetRemoveDouble( "mae_cutoff",  double.MaxValue);
+
+		// data arguments
+		string data_dir      = parameters.GetRemoveString( "data_dir"); Console.Error.WriteLine("data_dir {0}", data_dir);
+
+		// other arguments
+		save_model_file      = parameters.GetRemoveString( "save_model");
+		load_model_file      = parameters.GetRemoveString( "load_model");
+		int random_seed      = parameters.GetRemoveInt32(  "random_seed",  -1);
+		no_eval              = parameters.GetRemoveBool(   "no_eval",      false);
+		prediction_file = parameters.GetRemoveString( "predict_ratings_file"); // TODO
+		cross_validation     = parameters.GetRemoveInt32(  "cross_validation", 0);
+
+		if (random_seed != -1)
+			MyMediaLite.Util.Random.InitInstance(random_seed);
+
+		recommender = Recommender.CreateRatingPredictor(method);
+		if (recommender == null)
+			Usage(string.Format("Unknown method: '{0}'", method));
+
+		Recommender.Configure(recommender, parameters, Usage);
+
+		if (parameters.CheckForLeftovers())
+			Usage(-1);
+
+		// load all the data
+		TimeSpan loading_time = Utils.MeasureTime(delegate() {
+			LoadData(data_dir, track_no);
+		});
+		Console.WriteLine(string.Format(ni, "loading_time {0,0:0.##}", loading_time.TotalSeconds));
+
+		recommender.MinRating = min_rating;
+		recommender.MaxRating = max_rating;
+		Console.Error.WriteLine(string.Format(ni, "ratings range: [{0}, {1}]", recommender.MinRating, recommender.MaxRating));
+
+		if (track_no == 1)
+			DoTrack1();
+		else
+			DoTrack2();
+		
+		Console.Error.WriteLine("memory {0}", Memory.Usage);
+	}
+	
+	static void DoTrack2()
+	{
+		throw new NotImplementedException();
+	}
+	
+	static void DoTrack1()
+	{
+		Utils.DisplayDataStats(training_data, track1_test_data, recommender);		
+		
+		if (find_iter != 0)
+		{
+			if ( !(recommender is IIterativeModel) )
+				Usage("Only iterative recommenders support find_iter.");
+			IIterativeModel iterative_recommender = (MatrixFactorization) recommender;
+			Console.WriteLine(recommender.ToString() + " ");
+
+			if (load_model_file.Equals(string.Empty))
+				recommender.Train();
+			else
+				Recommender.LoadModel(iterative_recommender, load_model_file);
+
+			if (compute_fit)
+				Console.Write(string.Format(ni, "fit {0,0:0.#####} ", iterative_recommender.ComputeFit()));
+
+			DisplayResults(RatingEval.Evaluate(recommender, validation_data));
+			Console.WriteLine(" " + iterative_recommender.NumIter);
+
+			for (int i = iterative_recommender.NumIter + 1; i <= max_iter; i++)
+			{
+				TimeSpan time = Utils.MeasureTime(delegate() {
+					iterative_recommender.Iterate();
+				});
+				training_time_stats.Add(time.TotalSeconds);
+
+				if (i % find_iter == 0)
+				{
+					if (compute_fit)
+					{
+						double fit = 0;
+						time = Utils.MeasureTime(delegate() {
+							fit = iterative_recommender.ComputeFit();
+						});
+						fit_time_stats.Add(time.TotalSeconds);
+						Console.Write(string.Format(ni, "fit {0,0:0.#####} ", fit));
+					}
+
+					Dictionary<string, double> results = null;
+					time = Utils.MeasureTime(delegate() {
+						results = RatingEval.Evaluate(recommender, validation_data);
+						DisplayResults(results);
+						rmse_eval_stats.Add(results["RMSE"]);
+						Console.WriteLine(" " + i);
+					});
+					eval_time_stats.Add(time.TotalSeconds);
+
+					Recommender.SaveModel(recommender, save_model_file, i);
+
+					if (epsilon > 0 && results["RMSE"] > rmse_eval_stats.Min() + epsilon)
+					{
+						Console.Error.WriteLine(string.Format(ni, "{0} >> {1}", results["RMSE"], rmse_eval_stats.Min()));
+						Console.Error.WriteLine("Reached convergence on training/validation data after {0} iterations.", i);
+						break;
+					}
+					if (results["RMSE"] > rmse_cutoff || results["MAE"] > mae_cutoff)
+					{
+							Console.Error.WriteLine("Reached cutoff after {0} iterations.", i);
+							break;
+					}
+				}
+			} // for
+
+			DisplayIterationStats();
+			Recommender.SaveModel(recommender, save_model_file);
+		}
+		else
+		{
+			TimeSpan seconds;
+
+			if (load_model_file.Equals(string.Empty))
+			{
+				Console.Write(recommender.ToString());
+				if (cross_validation > 0)
+				{
+					Console.WriteLine();
+					var split = new RatingCrossValidationSplit(training_data, cross_validation);
+					var results = RatingEval.EvaluateOnSplit(recommender, split);
+					DisplayResults(results);
+					no_eval = true;
+					recommender.Ratings = training_data;
+				}
+				else
+				{
+					seconds = Utils.MeasureTime( delegate() { recommender.Train(); } );
+        			Console.Write(" training_time " + seconds + " ");
+					Recommender.SaveModel(recommender, save_model_file);
+				}
+			}
+			else
+			{
+				Recommender.LoadModel(recommender, load_model_file);
+				Console.Write(recommender.ToString() + " ");
+			}
+
+			if (!no_eval)
+			{
+				seconds = Utils.MeasureTime(
+			    	delegate()
+				    {
+						DisplayResults(RatingEval.Evaluate(recommender, validation_data));
+					}
+				);
+				Console.Write(" testing_time " + seconds);
+			}
+
+			if (!prediction_file.Equals(string.Empty))
+			{
+				seconds = Utils.MeasureTime(
+			    	delegate() {
+						Console.WriteLine();
+						KDDCup.PredictTrack1(recommender, track1_test_data, prediction_file);
+					}
+				);
+				Console.Error.Write("predicting_time " + seconds);
+			}
+
+			Console.WriteLine();
+		}
+	}
+
+    static void LoadData(string data_dir, uint track_no)
+	{
+		string training_file   = Path.Combine(data_dir, string.Format("trainIdx{0}.txt", track_no));
+		string test_file       = Path.Combine(data_dir, string.Format("testIdx{0}.txt",  track_no));
+		string validation_file = Path.Combine(data_dir, "validationIdx1.firstLines.txt");
+		string track_file      = Path.Combine(data_dir, string.Format("trackData{0}.txt",  track_no));
+		string album_file      = Path.Combine(data_dir, string.Format("albumData{0}.txt",  track_no));
+		string artist_file     = Path.Combine(data_dir, string.Format("artistData{0}.txt", track_no));
+		string genre_file      = Path.Combine(data_dir, string.Format("genreData{0}.txt",  track_no));
+		
+		// read training data
+		training_data = MyMediaLite.IO.KDDCup2011.Ratings.Read(training_file);
+		recommender.Ratings = training_data;
+
+		// read validation data, if necessary
+		if (track_no == 1)
+			validation_data = MyMediaLite.IO.KDDCup2011.Ratings.Read(validation_file);
+		
+		// read test data
+		if (track_no == 1)
+			track1_test_data = MyMediaLite.IO.KDDCup2011.Ratings.ReadTest(test_file);
+		else
+			track2_test_data = Track2Candidates.Read(test_file);
+		
+		// read item data
+		item_relations = Items.Read(track_file, album_file, artist_file, genre_file, track_no);
+	}
+
+	static void AbortHandler(object sender, ConsoleCancelEventArgs args)
+	{
+		DisplayIterationStats();
+	}
+
+	// TODO move to a class in the MyMediaLite base library
+	static void DisplayResults(Dictionary<string, double> result)
+	{
+		Console.Write(string.Format(ni, "RMSE {0,0:0.#####} MAE {1,0:0.#####} NMAE {2,0:0.#####}",
+		                            result["RMSE"], result["MAE"], result["NMAE"]));
+	}
+
+	static void DisplayIterationStats()
+	{
+		if (training_time_stats.Count > 0)
+			Console.Error.WriteLine(string.Format(
+			    ni,
+				"iteration_time: min={0,0:0.##}, max={1,0:0.##}, avg={2,0:0.##}",
+	            training_time_stats.Min(), training_time_stats.Max(), training_time_stats.Average()
+			));
+		if (eval_time_stats.Count > 0)
+			Console.Error.WriteLine(string.Format(
+			    ni,
+				"eval_time: min={0,0:0.##}, max={1,0:0.##}, avg={2,0:0.##}",
+	            eval_time_stats.Min(), eval_time_stats.Max(), eval_time_stats.Average()
+			));
+		if (compute_fit && fit_time_stats.Count > 0)
+			Console.Error.WriteLine(string.Format(
+			    ni,
+				"fit_time: min={0,0:0.##}, max={1,0:0.##}, avg={2,0:0.##}",
+            	fit_time_stats.Min(), fit_time_stats.Max(), fit_time_stats.Average()
+			));
+	}
+}
