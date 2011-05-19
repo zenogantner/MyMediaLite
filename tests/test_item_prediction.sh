99c8668df4e5fbf8e5dbd603029da1ab24d66795
Zeno Gantner <zeno.gantner@gmail.com>
2011-05-19 23:31:01 +0200

    update test scripts

--- tests/test_item_prediction.sh
+++ tests/test_item_prediction.sh
@@ -16,8 +16,8 @@ echo "--------------------"
 
 for method in ItemKNN WeightedItemKNN UserKNN WeightedUserKNN
 do
-	echo $PROGRAM $THIS_DIR/example.train $THIS_DIR/example.test $method k=20
-	     $PROGRAM $THIS_DIR/example.train $THIS_DIR/example.test $method k=20
+	echo $PROGRAM --training-file=$THIS_DIR/example.train --test-file=$THIS_DIR/example.test --recommender=$method --recommender-options="k=20"
+	     $PROGRAM --training-file=$THIS_DIR/example.train --test-file=$THIS_DIR/example.test --recommender=$method --recommender-options="k=20"
 done
 
 echo
@@ -28,21 +28,21 @@ DATA_DIR=../../../../data/ml100k
 
 for method in BPRMF WRMF
 do
-	echo $PROGRAM u1.base u1.test $method find_iter=1 max_iter=5 num_iter=1 data_dir=$DATA_DIR
-   	     $PROGRAM u1.base u1.test $method find_iter=1 max_iter=5 num_iter=1 data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --find-iter=1 --max-iter=5 --recommender-options="num_iter=1" --data-dir=$DATA_DIR
+   	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --find-iter=1 --max-iter=5 --recommender-options="num_iter=1" --data-dir=$DATA_DIR
 done
 
 
 for method in Random MostPopular
 do
-	echo $PROGRAM u1.base u1.test $method data_dir=$DATA_DIR
-	     $PROGRAM u1.base u1.test $method data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR
 done
 
 for method in ItemKNN WeightedItemKNN UserKNN WeightedUserKNN
 do
-	echo $PROGRAM u1.base u1.test $method k=20 data_dir=$DATA_DIR
-	     $PROGRAM u1.base u1.test $method k=20 data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=20" --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=20" --data-dir=$DATA_DIR
 done
 
 echo
@@ -53,20 +53,20 @@ DATA_DIR=../../../../data/ml1m
 
 for method in ItemAttributeKNN
 do
-	echo $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method item_attributes=item-attributes-genres.txt k=20 data_dir=$DATA_DIR
-	     $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method item_attributes=item-attributes-genres.txt k=20 data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --item-attributes=item-attributes-genres.txt --recommender-options="k=20" --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --item-attributes=item-attributes-genres.txt --recommender-options="k=20" --data-dir=$DATA_DIR
 done
 
 for method in BPR_Linear
 do
-	echo $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method item_attributes=item-attributes-genres.txt find_iter=1 max_iter=2 num_iter=1 data_dir=$DATA_DIR
-	     $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method item_attributes=item-attributes-genres.txt find_iter=1 max_iter=2 num_iter=1 data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --item-attributes=item-attributes-genres.txt --find-iter=1 --max-iter=2 --recommender-options="num_iter=1" --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --item-attributes=item-attributes-genres.txt --find-iter=1 --max-iter=2 --recommender-options="num_iter=1" --data-dir=$DATA_DIR
 done
 
 for method in UserAttributeKNN
 do
-	echo $PROGRAM ml1m-new-user-0.train.txt ml1m-new-user-0.test.txt $method user_attributes=user-attributes-nozip.txt k=20 data_dir=$DATA_DIR
-             $PROGRAM ml1m-new-user-0.train.txt ml1m-new-user-0.test.txt $method user_attributes=user-attributes-nozip.txt k=20 data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=ml1m-new-user-0.train.txt --test-file=ml1m-new-user-0.test.txt --recommender=$method --user-attributes=user-attributes-nozip.txt --recommender-options="k=20" --data-dir=$DATA_DIR
+             $PROGRAM --training-file=ml1m-new-user-0.train.txt --test-file=ml1m-new-user-0.test.txt --recommender=$method --user-attributes=user-attributes-nozip.txt --recommender-options="k=20" --data-dir=$DATA_DIR
 done
 
 cd ../../../../
--- tests/test_rating_prediction.sh
+++ tests/test_rating_prediction.sh
@@ -15,28 +15,28 @@ cd src/RatingPrediction/bin/Debug/
  
 for method in MatrixFactorization BiasedMatrixFactorization
 do
-       echo $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method find_iter=1 max_iter=5 num_iter=1 compute_fit=true data_dir=$DATA_DIR
-       #     $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method find_iter=1 max_iter=5 num_iter=1 compute_fit=true data_dir=$DATA_DIR
+       echo $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --find-iter=1 --max-iter=5 --recommender-options="num_iter=1" --compute-fit --data-dir=$DATA_DIR
+            $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --find-iter=1 --max-iter=5 --recommender-options="num_iter=1" --compute-fit --data-dir=$DATA_DIR
 done
  
 touch $DATA_DIR/empty
 for method in SocialMF
 do
-	echo $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method find_iter=1 max_iter=15 num_iter=1 learn_rate=0.005 compute_fit=true social_reg=0 data_dir=$DATA_DIR user_relation=empty
-	     $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method find_iter=1 max_iter=15 num_iter=1 learn_rate=0.005 compute_fit=true social_reg=0 data_dir=$DATA_DIR user_relation=empty
+	echo $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --find-iter=1 --max-iter=15 --recommender-options="num_iter=1 learn_rate=0.005 social_reg=0" --compute-fit --data-dir=$DATA_DIR --user-relations=empty
+	     $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --find-iter=1 --max-iter=15 --recommender-options="num_iter=1 learn_rate=0.005 social_reg=0" --compute-fit --data-dir=$DATA_DIR --user-relations=empty
 done
 rm $DATA_DIR/empty
 
 for method in UserItemBaseline GlobalAverage UserAverage ItemAverage
 do
-	echo $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method data_dir=$DATA_DIR
-	     $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --data-dir=$DATA_DIR
 done
 
 for method in ItemAttributeKNN
 do
-	echo $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method item_attributes=item-attributes-genres.txt k=20 data_dir=$DATA_DIR
-	     $PROGRAM ml1m-0.train.txt ml1m-0.test.txt $method item_attributes=item-attributes-genres.txt k=20 data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --item-attributes=item-attributes-genres.txt --recommender-options="k=20" --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=ml1m-0.train.txt --test-file=ml1m-0.test.txt --recommender=$method --item-attributes=item-attributes-genres.txt --recommender-options="k=20" --data-dir=$DATA_DIR
 done
 
 
@@ -48,14 +48,14 @@ DATA_DIR=../../../../data/ml100k
 
 for method in UserItemBaseline SlopeOne BipolarSlopeOne
 do
-	echo $PROGRAM u1.base u1.test $method data_dir=$DATA_DIR
-	     $PROGRAM u1.base u1.test $method data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR
 done
 
 for method in UserKNNPearson UserKNNCosine ItemKNNPearson ItemKNNCosine
 do
-	echo $PROGRAM u1.base u1.test $method k=20 data_dir=$DATA_DIR
-	     $PROGRAM u1.base u1.test $method k=20 data_dir=$DATA_DIR
+	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=20" --data-dir=$DATA_DIR
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=20" --data-dir=$DATA_DIR
 done
 
 cd ../../../../
