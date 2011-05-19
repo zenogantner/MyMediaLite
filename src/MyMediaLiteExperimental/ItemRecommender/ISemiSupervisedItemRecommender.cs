17d1af34dcc113b2a4ffaf82c23ad27dce6376e0
Zeno Gantner <zeno.gantner@gmail.com>
2011-05-19 12:02:45 +0200

    monodevelop configuration

---
+++ src/MyMediaLiteExperimental/ItemRecommender/ISemiSupervisedItemRecommender.cs
@@ -0,0 +1,33 @@
+// Copyright (C) 2011 Zeno Gantner
+// 
+// This file is part of MyMediaLite.
+// 
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+
+namespace MyMediaLite.ItemRecommendation
+{
+	// TODO handle this interface in the item recommendation command-line program
+	
+	/// <summary>Interface for item recommenders that take into account some test data for training</summary>
+	public interface ISemiSupervisedItemRecommender
+	{
+		/// <summary>Set containing all test users</summary>
+		HashSet<int> TestUsers { get; set; }
+
+		/// <summary>Set containing all test items</summary>
+		HashSet<int> TestItems { get; set; }		
+	}
+}
