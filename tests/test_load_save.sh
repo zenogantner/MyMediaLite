68b6c73183ff1c4e7ddb90780070087d203a305d
Zeno Gantner <zeno.gantner@gmail.com>
2011-07-07 14:31:32 +0200

    test: change time estimate

--- tests/test_load_save.sh
+++ tests/test_load_save.sh
@@ -7,7 +7,7 @@ DATA_DIR=../../../../data/ml100k
 
 cd src/RatingPrediction/bin/Debug/
 
-echo "This will take about 5 minutes ..."
+echo "This will take about 10 minutes ..."
 
 echo
 echo "rating predictors"
