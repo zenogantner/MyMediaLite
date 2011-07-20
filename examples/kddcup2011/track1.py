b86b2760014ae3bf8788d6983e471dce1a9e9c91
Zeno Gantner <zeno.gantner@gmail.com>
2011-07-20 11:42:09 +0200

    fix KDD Cup example track 1

--- examples/kddcup2011/track1.py
+++ examples/kddcup2011/track1.py
@@ -10,9 +10,9 @@ validation_file = "validationIdx1.firstLines.txt"
 test_file       = "testIdx1.firstLines.txt"
 
 # load the data
-training_data   = IO.KDDCup2011.Ratings.Read(train_file, 11696)
-validation_data = IO.KDDCup2011.Ratings.Read(validation_file, 220)
-test_data       = IO.KDDCup2011.Ratings.ReadTest(test_file, 308)
+training_data   = IO.KDDCup2011.Ratings.Read(train_file)
+validation_data = IO.KDDCup2011.Ratings.Read(validation_file)
+test_data       = IO.KDDCup2011.Ratings.ReadTest(test_file)
 
 item_relations = IO.KDDCup2011.Items.Read("trackData1.txt", "albumData1.txt", "artistData1.txt", "genreData1.txt", 1);
 print item_relations
@@ -27,9 +27,9 @@ recommender.Train()
 print "done."
 
 # measure the accuracy on the validation set
-print Eval.RatingEval.Evaluate(recommender, validation_data)
+print Eval.Ratings.Evaluate(recommender, validation_data)
 
 # predict on the test set
 print "Predicting ..."
-Eval.KDDCup.PredictTrack1(recommender, test_data, "track1-output.txt")
+Eval.KDDCup.PredictRatingsDouble(recommender, test_data, "track1-output.txt")
 print "done."
