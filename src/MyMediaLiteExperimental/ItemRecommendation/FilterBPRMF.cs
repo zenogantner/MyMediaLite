d250a1734678676cae19710b65d2e40883561b6d
Zeno Gantner <zeno.gantner@gmail.com>
2011-08-04 14:43:30 +0200

    add weighted and filtered BPRMF

--- src/MyMediaLiteExperimental/ItemRecommendation/FilterBPRMF.cs
+++ src/MyMediaLiteExperimental/ItemRecommendation/FilterBPRMF.cs
@@ -1,25 +1,83 @@
 // Copyright (C) 2011 Zeno Gantner
-// 
+//
 // This file is part of MyMediaLite.
-// 
+//
 // MyMediaLite is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // MyMediaLite is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU General Public License
 //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyMediaLite.DataType;
+using MyMediaLite.Eval;
 
 namespace MyMediaLite.ItemRecommendation
 {
+	/// <summary>Matrix factroization optimized for attribute-filtered BPR</summary>
 	public class FilterBPRMF : BPRMF, IItemAttributeAwareRecommender
 	{
+		///
+		public SparseBooleanMatrix ItemAttributes
+		{
+			get { return this.item_attributes; }
+			set	{
+				this.item_attributes = value;
+				this.NumItemAttributes = item_attributes.NumberOfColumns;
+				this.MaxItemID = Math.Max(MaxItemID, item_attributes.NumberOfRows - 1);
+			}
+		}
+		private SparseBooleanMatrix item_attributes;
+
+		///
+		public int NumItemAttributes { get;	private set; }
+
+		// TODO document
+		IList<Dictionary<int, ICollection<int>>> filtered_items_by_user;
+		SparseBooleanMatrix items_by_attribute;
+
+		///
+		public override void Train()
+		{
+			filtered_items_by_user = new Dictionary<int, ICollection<int>>[MaxUserID + 1];
+			items_by_attribute = (SparseBooleanMatrix) item_attributes.Transpose();
+
+			Console.Error.WriteLine("max_user_id {0} max_item_id {1}", MaxUserID, MaxItemID);
+
+			for (int u = 0; u < filtered_items_by_user.Count; u++)
+				filtered_items_by_user[u] = ItemsFiltered.GetFilteredItems(u, Feedback, ItemAttributes);
+
+			base.Train();
+		}
+
+		/// <summary>Sample an item pair for a given user</summary>
+		/// <param name="u">the user ID</param>
+		/// <param name="i">the ID of the higher-ranking item</param>
+		/// <param name="j">the ID of the lower-ranking item</param>
+		protected override void SampleItemPair(int u, out int i, out int j)
+		{
+			// sample filter attribute accessed by user
+			var user_filter_attributes = filtered_items_by_user[u].Keys;
+			int a = user_filter_attributes.ElementAt(random.Next(0, user_filter_attributes.Count));
+
+			// TODO catch condition that user has rated all comedies ...
+
+			var user_filtered_items = filtered_items_by_user[u][a];
+			// sample positive item
+			i = user_filtered_items.ElementAt(random.Next(0, user_filtered_items.Count));
+			// sample negative item
+			do
+				j = items_by_attribute[a].ElementAt(random.Next(0, items_by_attribute[a].Count));
+			while (Feedback.UserMatrix[u, j]);
+		}
 	}
 }
-
---
+++ src/MyMediaLiteExperimental/ItemRecommendation/WeightedBPRMF.cs
@@ -0,0 +1,90 @@
+// Copyright (C) 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using MyMediaLite.DataType;
+using MyMediaLite.Util;
+
+namespace MyMediaLite.ItemRecommendation
+{
+	/// <summary>Weigthed BPR-MF with frequency-adjusted sampling</summary>
+	/// <remarks>
+	/// Zeno Gantner, Lucas Drumond, Christoph Freudenthaler, Lars Schmidt-Thieme:
+	/// Bayesian Personalized Ranking for Non-Uniformly Sampled Items.
+	/// KDD Cup Workshop 2011
+	/// </remarks>
+	public class WeightedBPRMF : BPRMF
+	{
+		// TODO offer this data structure from Feedback
+		/// <summary>array of user IDs of positive user-item pairs</summary>
+		protected int[] users;
+		/// <summary>array of item IDs of positive user-item pairs</summary>
+		protected int[] items;
+
+		/// <summary>Default constructor</summary>
+		public WeightedBPRMF() { }
+
+		///
+		public override void Train()
+		{
+			// prepare helper data structures for training
+			users = new int[Feedback.Count];
+			items = new int[Feedback.Count];
+
+			int index = 0;
+			foreach (int user_id in Feedback.UserMatrix.NonEmptyRowIDs)
+				foreach (int item_id in Feedback.UserMatrix[user_id])
+				{
+					users[index] = user_id;
+					items[index] = item_id;
+
+					index++;
+				}
+
+			// suppress using user_neg_items in BPRMF
+			FastSamplingMemoryLimit = 0;
+
+			base.Train();
+		}
+
+		///
+		protected override void SampleTriple(out int u, out int i, out int j)
+		{
+			// sample user from positive user-item pairs
+			int index = random.Next(0, items.Length - 1);
+			u = users[index];
+			i = items[index];
+
+			// sample negative item
+			do
+				j = items[random.Next(0, items.Length - 1)];
+			while (Feedback.UserMatrix[u, j]);
+		}
+
+		///
+		public override string ToString()
+		{
+			return string.Format(CultureInfo.InvariantCulture,
+			                     "WeightedBPRMF num_factors={0} bias_reg={1} reg_u={2} reg_i={3} reg_j={4} num_iter={5} bold_driver={6} learn_rate={7} init_mean={8} init_stdev={8}",
+								 num_factors, BiasReg, reg_u, reg_i, reg_j, NumIter, BoldDriver, learn_rate, InitMean, InitStdev);
+		}
+	}
+}
+
