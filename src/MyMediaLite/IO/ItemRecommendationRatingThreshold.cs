34ebbb9765d36a0e80641139c87f36191755f932
Zeno Gantner <zeno.gantner@gmail.com>
2011-08-08 14:43:35 +0200

    add missing files

---
+++ src/MyMediaLite/IO/ItemRecommendationRatingThreshold.cs
@@ -0,0 +1,104 @@
+// Copyright (C) 2010, 2011 Zeno Gantner
+// Copyright (C) 2011 Artus Krohn-Grimberghe
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using MyMediaLite.Data;
+using MyMediaLite.DataType;
+using MyMediaLite.Util;
+
+namespace MyMediaLite.IO
+{
+	/// <summary>Class that contains static methods for reading in implicit feedback data for ItemRecommender</summary>
+	public class ItemRecommendationRatingThreshold
+	{
+		/// <summary>Read in rating data which will be interpreted as implicit feedback data from a file</summary>
+		/// <param name="filename">name of the file to be read from</param>
+		/// <param name="rating_threshold">the minimum rating value needed to be accepted as positive feedback</param>
+		/// <param name="user_mapping">user <see cref="IEntityMapping"/> object</param>
+		/// <param name="item_mapping">item <see cref="IEntityMapping"/> object</param>
+		/// <returns>a <see cref="IPosOnlyFeedback"/> object with the user-wise collaborative data</returns>
+		static public IPosOnlyFeedback Read(string filename, double rating_threshold, IEntityMapping user_mapping, IEntityMapping item_mapping)
+		{
+           	using ( var reader = new StreamReader(filename) )
+				return Read(reader, rating_threshold, user_mapping, item_mapping);
+		}
+
+		/// <summary>Read in rating data which will be interpreted as implicit feedback data from a TextReader</summary>
+		/// <param name="reader">the TextReader to be read from</param>
+		/// <param name="rating_threshold">the minimum rating value needed to be accepted as positive feedback</param>
+		/// <param name="user_mapping">user <see cref="IEntityMapping"/> object</param>
+		/// <param name="item_mapping">item <see cref="IEntityMapping"/> object</param>
+		/// <returns>a <see cref="IPosOnlyFeedback"/> object with the user-wise collaborative data</returns>
+		static public IPosOnlyFeedback Read(TextReader reader, double rating_threshold, IEntityMapping user_mapping, IEntityMapping item_mapping)
+		{
+	        var feedback = new PosOnlyFeedback<SparseBooleanMatrix>();
+
+			var split_chars = new char[]{ '\t', ' ', ',' };
+			string line;
+
+			while ( (line = reader.ReadLine()) != null )
+			{
+				if (line.Trim().Length == 0)
+					continue;
+
+	            string[] tokens = line.Split(split_chars);
+
+				if (tokens.Length < 3)
+					throw new IOException("Expected at least 3 columns: " + line);
+
+				int user_id   = user_mapping.ToInternalID(int.Parse(tokens[0]));
+				int item_id   = item_mapping.ToInternalID(int.Parse(tokens[1]));
+				double rating = double.Parse(tokens[2], CultureInfo.InvariantCulture);
+
+				if (rating >= rating_threshold)
+               		feedback.Add(user_id, item_id);
+			}
+
+			return feedback;
+		}
+
+        /// <summary>Read in rating data which will be interpreted as implicit feedback data from an IDataReader, e.g. a database via DbDataReader</summary>
+		/// <param name="reader">the IDataReader to be read from</param>
+		/// <param name="rating_threshold">the minimum rating value needed to be accepted as positive feedback</param>
+        /// <param name="user_mapping">user <see cref="IEntityMapping"/> object</param>
+        /// <param name="item_mapping">item <see cref="IEntityMapping"/> object</param>
+        /// <returns>a <see cref="IPosOnlyFeedback"/> object with the user-wise collaborative data</returns>
+        static public IPosOnlyFeedback Read(IDataReader reader, double rating_threshold, IEntityMapping user_mapping, IEntityMapping item_mapping)
+        {
+            var feedback = new PosOnlyFeedback<SparseBooleanMatrix>();
+
+            if (reader.FieldCount < 3)
+                throw new IOException("Expected at least 3 columns.");
+
+            while (reader.Read())
+            {
+                int user_id = user_mapping.ToInternalID(reader.GetInt32(0));
+                int item_id = item_mapping.ToInternalID(reader.GetInt32(1));
+				double rating = reader.GetDouble(2);
+				
+				if (rating >= rating_threshold)
+                	feedback.Add(user_id, item_id);
+            }
+
+            return feedback;
+        }
+	}
+}
