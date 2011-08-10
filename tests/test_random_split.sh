74cff2a9f4ca425358c82922c586b5de24e03b08
Zeno Gantner <zeno.gantner@gmail.com>
2011-08-10 15:00:31 +0200

    one more test case for random splits

--- tests/test_random_split.sh
+++ tests/test_random_split.sh
@@ -46,4 +46,18 @@ diff log1.grep log2.grep
 
 rm log1 log2 log1.grep log2.grep
 
+echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=MostPopular --data-dir=$DATA_DIR --random-seed=1 --num-test-users=100 > log1
+     $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=MostPopular --data-dir=$DATA_DIR --random-seed=1 --num-test-users=100 > log1
+
+echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=WRMF --data-dir=$DATA_DIR --random-seed=1 --num-test-users=100 > log2
+     $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=WRMF --data-dir=$DATA_DIR --random-seed=1 --num-test-users=100 > log2
+
+grep "100 users" log1 > log1.grep
+grep "100 users" log2 > log2.grep
+
+diff log1.grep log2.grep
+
+rm log1 log2 log1.grep log2.grep
+
+
 cd ../../../../
