aa562b82427ac109559bad2e43e4258a7aca5c78
Zeno Gantner <zeno.gantner@gmail.com>
2011-12-03 15:24:15 +0100

    add dense SymmetricMatrix datatype

---
+++ src/MyMediaLite/DataType/SymmetricMatrix.cs
@@ -0,0 +1,115 @@
+// Copyright (C) 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+
+namespace MyMediaLite.DataType
+{
+	/// <summary>Class for storing dense matrices</summary>
+	/// <remarks>
+	/// The data is stored in row-major mode.
+	/// Indexes are zero-based.
+	/// </remarks>
+	/// <typeparam name="T">the type of the matrix entries</typeparam>
+	public class SymmetricMatrix<T> : IMatrix<T>
+	{
+		/// <summary>Data array: data is stored in columns.</summary>
+		protected internal T[][] data;
+		/// <summary>Dimension, the number of rows and columns</summary>
+		public int dim;
+
+		///
+		public virtual bool IsSymmetric
+		{
+			get { return true; }
+		}
+
+		///
+		public int NumberOfRows { get { return dim; } }
+
+		///
+		public int NumberOfColumns { get { return dim; } }
+
+		/// <summary>Initializes a new instance of the SymmetricMatrix class</summary>
+		/// <param name="dim">the number of rows and columns</param>
+		public SymmetricMatrix(int dim)
+		{
+			if (dim < 0)
+				throw new ArgumentOutOfRangeException("dim must be at least 0");
+
+			this.dim = dim;
+			this.data = new T[dim][];
+			for (int i = 0; i < dim; i++)
+				data[i] = new T[dim - i];
+		}
+
+		///
+		public IMatrix<T> CreateMatrix(int num_rows, int num_columns)
+		{
+			if (num_rows != num_columns)
+				throw new ArgumentException("num_rows must equal num_columns for symmetric matrices");
+			return new SymmetricMatrix<T>(num_rows);
+		}
+
+		///
+		public IMatrix<T> Transpose()
+		{
+			throw new NotImplementedException();
+		}
+
+		///
+		public virtual T this [int i, int j]
+		{
+			get {
+				if (i <= j)
+					return data[i][j];
+				else
+					return data[j][i];
+			}
+			set {
+				if (i <= j)
+					data[i][j] = value;
+				else
+					data[j][i] = value;
+			}
+		}
+
+		///
+		public void Grow(int num_rows, int num_columns)
+		{
+			if (num_rows != num_columns)
+				throw new ArgumentException("num_rows must equal num_columns for symmetric matrices");
+
+			if (num_rows > dim)
+			{
+				// create new data structure
+				var new_data = new T[num_rows][];
+				for (int i = 0; i < num_rows; i++)
+					new_data[i] = new T[num_rows - i];
+
+				for (int i = 0; i < dim; i++)
+					for (int j = i; j < dim; j++)
+						new_data[i][j] = this[i, j];
+
+				// replace old data structure
+				this.dim = num_rows;
+				this.data = new_data;
+			}
+		}
+	}
+}
---
+++ src/Tests/DataType/SymmetricMatrixTest.cs
@@ -0,0 +1,65 @@
+// Copyright (C) 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using MyMediaLite.DataType;
+using NUnit.Framework;
+
+namespace Tests.DataType
+{
+	/// <summary>Tests for the SymmetricMatrix<T> class</summary>
+	[TestFixture()]
+	public class SymmetricMatrixTest
+	{
+		[Test()] public void TestSymmetricity()
+		{
+			var matrix = new SymmetricMatrix<double>(5);
+			matrix[1, 3] = 1.0;
+
+			Assert.AreEqual(1.0, matrix[3, 1]);
+		}
+
+		[Test()] public void TestIsSymmetric()
+		{
+			var matrix = new SymmetricMatrix<double>(5);
+			Assert.IsTrue(matrix.IsSymmetric);
+
+			matrix[1, 3] = 1.0;
+			Assert.IsTrue(matrix.IsSymmetric);
+		}
+
+		[Test()] public void TestNumberOfRows()
+		{
+			var matrix = new SymmetricMatrix<double>(3);
+			Assert.AreEqual(3, matrix.NumberOfRows);
+		}
+
+		[Test()] public void TestNumberOfColumns()
+		{
+			var matrix = new SymmetricMatrix<double>(3);
+			Assert.AreEqual(3, matrix.NumberOfColumns);
+		}
+
+		[Test()] public void TestCreateMatrix()
+		{
+			var matrix1 = new SymmetricMatrix<double>(5);
+			var matrix2 = matrix1.CreateMatrix(4, 4);
+			Assert.IsInstanceOfType(matrix1.GetType(), matrix2);
+		}
+	}
+}
