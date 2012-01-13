a220c8e97629047bb62ef996ee138d4d1a3b72f8
Zeno Gantner <zeno.gantner@gmail.com>
2012-01-13 10:35:46 +0100

    UserAttributeKNN: change formatting, update (c) year

--- src/MyMediaLite/RatingPrediction/UserAttributeKNN.cs
+++ src/MyMediaLite/RatingPrediction/UserAttributeKNN.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2010, 2011 Zeno Gantner
+// Copyright (C) 2010, 2011, 2012 Zeno Gantner
 //
 // This file is part of MyMediaLite.
 //
@@ -35,7 +35,7 @@ namespace MyMediaLite.RatingPrediction
 		public SparseBooleanMatrix UserAttributes
 		{
 			get { return this.user_attributes; }
-			set	{
+			set {
 				this.user_attributes = value;
 				this.NumUserAttributes = user_attributes.NumberOfColumns;
 				this.MaxUserID = Math.Max(MaxUserID, user_attributes.NumberOfRows - 1);
