fb123e796cd89e6a0715e66e7964c1594b9c01dc
Zeno Gantner <zeno.gantner@gmail.com>
2012-01-25 18:22:03 +0100

    add Remove(At|User|Item) to RatingsProxy

--- src/MyMediaLite/Data/RatingsProxy.cs
+++ src/MyMediaLite/Data/RatingsProxy.cs
@@ -25,19 +25,82 @@ namespace MyMediaLite.Data
 	/// <summary>Data structure that allows access to selected entries of a rating data structure</summary>
 	public class RatingsProxy : Ratings
 	{
+		IList<int> indices;
+
 		/// <summary>Create a RatingsProxy object</summary>
 		/// <param name="ratings">a ratings data structure</param>
 		/// <param name="indices">an index list pointing to entries in the ratings</param>
 		public RatingsProxy(IRatings ratings, IList<int> indices)
 		{
+			this.indices = indices;
+
 			Users  = new ListProxy<int>(ratings.Users, indices);
 			Items  = new ListProxy<int>(ratings.Items, indices);
 			Values = new ListProxy<float>(ratings, indices);
 
-			MaxUserID = ratings.MaxUserID;
-			MaxItemID = ratings.MaxItemID;
+			MaxUserID = -1;
+			MaxItemID = -1;
 			MaxRating = ratings.MaxRating;
 			MinRating = ratings.MinRating;
+
+			foreach (int index in indices)
+			{
+				if (ratings.Users[index] > MaxUserID)
+					MaxUserID = ratings.Users[index];
+				if (ratings.Items[index] > MaxItemID)
+					MaxItemID = ratings.Items[index];
+			}
+		}
+
+		///
+		public override void RemoveAt(int index)
+		{
+			int user_id = Users[index];
+			int item_id = Items[index];
+
+			Console.Error.WriteLine("at " + index);
+
+			indices.RemoveAt(index);
+			UpdateCountsAndIndices(new HashSet<int>() { user_id }, new HashSet<int>() { item_id });
+		}
+
+		///
+		public override void RemoveUser(int user_id)
+		{
+			var items_to_update = new HashSet<int>();
+
+			for (int index = 0; index < Count; index++)
+				if (Users[index] == user_id)
+				{
+					Console.Error.WriteLine("remove {0}: {1} {2} {3}", index, Users[index], Items[index], Values[index]);
+					items_to_update.Add(Items[index]);
+					indices.RemoveAt(index);
+					index--; // avoid missing an entry
+				}
+
+			UpdateCountsAndIndices(new HashSet<int>() { user_id }, items_to_update);
+
+			if (MaxUserID == user_id)
+				MaxUserID--;
+		}
+
+		///
+		public override void RemoveItem(int item_id)
+		{
+			var users_to_update = new HashSet<int>();
+
+			for (int index = 0; index < Count; index++)
+				if (Items[index] == item_id)
+				{
+					users_to_update.Add(Users[index]);
+					indices.RemoveAt(index);
+					index--; // avoid missing an entry
+				}
+
+			UpdateCountsAndIndices(users_to_update, new HashSet<int>() { item_id });
+
+			if (MaxItemID == item_id)
+				MaxItemID--;
 		}
 	}
 }
--- src/Tests/Data/RatingsProxyTest.cs
+++ src/Tests/Data/RatingsProxyTest.cs
@@ -16,6 +16,7 @@
 //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Linq;
 using NUnit.Framework;
 using MyMediaLite.Data;
 
@@ -33,6 +34,7 @@ namespace Tests.Data
 			ratings.Add(2, 2, 0.6f);
 			ratings.Add(2, 5, 0.4f);
 			ratings.Add(3, 7, 0.2f);
+			ratings.Add(3, 3, 0.3f);
 			ratings.Add(6, 3, 0.3f);
 
 			return ratings;
@@ -67,6 +69,93 @@ namespace Tests.Data
 			var ratings = new RatingsProxy(CreateRatings(), new int[] { 1, 3, 5 });
 
 			Assert.AreEqual(3, ratings.Count);
+		}
+
+		[Test()] public void TestRemoveAt()
+		{
+			var ratings = new RatingsProxy(CreateRatings(), Enumerable.Range(0, 8).ToList());
+
+			Assert.AreEqual(8, ratings.Count);
+			Assert.AreEqual(0.4f, ratings[2, 5]);
+			Assert.AreEqual(3, ratings.ByUser[2].Count);
+			Assert.AreEqual(1, ratings.ByUser[6].Count);
+			Assert.AreEqual(2, ratings.ByItem[3].Count);
+			Assert.AreEqual(1, ratings.ByItem[5].Count);
+			Assert.AreEqual(3, ratings.CountByUser[2]);
+			Assert.AreEqual(1, ratings.CountByUser[6]);
+			Assert.AreEqual(2, ratings.CountByItem[3]);
+			Assert.AreEqual(1, ratings.CountByItem[5]);
+
+			ratings.RemoveAt(ratings.GetIndex(2, 5));
+			Assert.AreEqual(7, ratings.Count);
+			Assert.AreEqual(2, ratings.ByUser[2].Count);
+			Assert.AreEqual(0, ratings.ByItem[5].Count);
+			Assert.AreEqual(2, ratings.CountByUser[2]);
+			Assert.AreEqual(0, ratings.CountByItem[5]);
+
+			ratings.RemoveAt(ratings.GetIndex(6, 3));
+			Assert.AreEqual(6, ratings.Count);
+			Assert.AreEqual(0, ratings.ByUser[6].Count);
+			Assert.AreEqual(1, ratings.ByItem[3].Count);
+			Assert.AreEqual(0, ratings.CountByUser[6]);
+			Assert.AreEqual(1, ratings.CountByItem[3]);
+
+			float r;
+			Assert.IsFalse(ratings.TryGet(2, 5, out r));
+		}
+
+		[Test()] public void TestRemoveUser()
+		{
+			var ratings = new RatingsProxy(CreateRatings(), Enumerable.Range(0, 8).ToList());
+
+			Assert.AreEqual(8, ratings.Count);
+			Assert.AreEqual(0.4f, ratings[2, 5]);
+			Assert.AreEqual(1, ratings.ByItem[2].Count);
+			Assert.AreEqual(2, ratings.ByItem[4].Count);
+			Assert.AreEqual(1, ratings.ByItem[5].Count);
+			Assert.AreEqual(1, ratings.CountByItem[2]);
+			Assert.AreEqual(2, ratings.CountByItem[4]);
+			Assert.AreEqual(1, ratings.CountByItem[5]);
+
+			ratings.RemoveUser(2);
+			Assert.AreEqual(5, ratings.Count);
+			Assert.AreEqual(0, ratings.ByItem[2].Count);
+			Assert.AreEqual(1, ratings.ByItem[4].Count);
+			Assert.AreEqual(0, ratings.ByItem[5].Count);
+			Assert.AreEqual(0, ratings.CountByItem[2]);
+			Assert.AreEqual(1, ratings.CountByItem[4]);
+			Assert.AreEqual(0, ratings.CountByItem[5]);
+
+			float rating;
+			Assert.IsFalse(ratings.TryGet(2, 5, out rating));
+			Assert.IsFalse(ratings.TryGet(2, 4, out rating));
+			Assert.IsFalse(ratings.TryGet(2, 2, out rating));
+		}
+
+		[Test()] public void TestRemoveItem()
+		{
+			var ratings = new RatingsProxy(CreateRatings(), Enumerable.Range(0, 8).ToList());
+
+			Assert.AreEqual(8, ratings.Count);
+			Assert.AreEqual(0.2f, ratings[2, 4]);
+			Assert.AreEqual(2, ratings.ByUser[1].Count);
+			Assert.AreEqual(3, ratings.ByUser[2].Count);
+			Assert.AreEqual(2, ratings.ByUser[3].Count);
+			Assert.AreEqual(2, ratings.CountByUser[1]);
+			Assert.AreEqual(3, ratings.CountByUser[2]);
+			Assert.AreEqual(2, ratings.CountByUser[3]);
+
+			ratings.RemoveItem(4);
+			Assert.AreEqual(6, ratings.Count);
+			Assert.AreEqual(1, ratings.ByUser[1].Count);
+			Assert.AreEqual(2, ratings.ByUser[2].Count);
+			Assert.AreEqual(1, ratings.CountByUser[1]);
+			Assert.AreEqual(2, ratings.CountByUser[2]);
+
+			float r;
+			Assert.IsFalse(ratings.TryGet(1, 4, out r));
+			Assert.IsFalse(ratings.TryGet(2, 4, out r));
+			Assert.IsFalse(ratings.TryGet(3, 4, out r));
 		}
 
 		[Test()]
