3612f742fe68151f1224e97826fd595df78557ef
Zeno Gantner <zeno.gantner@gmail.com>
2012-01-26 16:09:18 +0100

    update fold-in example

--- examples/csharp/foldin_incremental.cs
+++ examples/csharp/foldin_incremental.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using MyMediaLite.Data;
 using MyMediaLite.Eval;
 using MyMediaLite.IO;
@@ -10,64 +11,57 @@ public class FoldInAndIncrementalTraining
 {
 	public static void Main(string[] args)
 	{
-		// TODO user-wise leave-one out
-		// TODO use entirely new users
+		// TODO add random seed
 		// TODO make recommender type configurable
 		// TODO report per-user times
 
 		// load the data
-		var training_data = RatingData.Read(args[0]);
-		var test_data = RatingData.Read(args[1]);
+		var all_data = RatingData.Read(args[0]);
+
+		// TODO randomize
+		var test_users = new HashSet<int>(Enumerable.Range(0, 200));
+
 		var update_indices = new List<int>();
 		var eval_indices = new List<int>();
-		foreach (int user_id in test_data.AllUsers)
-			if (test_data.ByUser[user_id].Count > 1)
+		foreach (int user_id in test_users)
+			if (all_data.ByUser[user_id].Count > 1)
 			{
-				var user_indices = test_data.ByUser[user_id];
+				var user_indices = all_data.ByUser[user_id];
 				for (int i = 0; i < user_indices.Count - 1; i++)
 					update_indices.Add(user_indices[i]);
 				for (int i = user_indices.Count - 1; i < user_indices.Count; i++)
 					eval_indices.Add(user_indices[i]);
 			}
 
-		var update_data = new RatingsProxy(test_data, update_indices);
-		var eval_data   = new RatingsProxy(test_data, eval_indices);
+		var training_indices = new List<int>();
+		for (int i = 0; i < all_data.Count; i++)
+			if (!test_users.Contains(all_data.Users[i]))
+				training_indices.Add(i);
+		var training_data = new MyMediaLite.Data.Ratings();
+		foreach (int i in training_indices)
+			training_data.Add(all_data.Users[i], all_data.Items[i], all_data[i]);
+
+		var update_data = new RatingsProxy(all_data, update_indices);
+		var eval_data   = new RatingsProxy(all_data, eval_indices);
 
 		Console.Write(training_data.Statistics());
 		Console.Write(update_data.Statistics());
         Console.Write(eval_data.Statistics());
 
-		// I. complete retraining
-		// Ia. prepare
+		// prepare recommender
 		var recommender = new MatrixFactorization();
-		recommender.Ratings = new CombinedRatings(training_data, update_data);
+		recommender.Ratings = training_data;
 		Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "ratings range: [{0}, {1}]", recommender.MinRating, recommender.MaxRating));
 		Console.WriteLine("recommender: {0}", recommender);
 		recommender.Train();
-		// Ib. evaluate
+
+		// I. complete retraining
 		Console.WriteLine("complete training: {0}", recommender.EvaluateFoldInCompleteRetraining(update_data, eval_data));
 
 		// II. online updates
-		// IIa. prepare
-		recommender = new MatrixFactorization();
-		recommender.Ratings = training_data;
-		recommender.Train();
-		// Ib. train and evaluate
 		Console.WriteLine("incremental training: {0}", recommender.EvaluateFoldInIncrementalTraining(update_data, eval_data));
 
 		// III. fold-in
-		// IIIa. prepare
-		var foldin_recommender = new MatrixFactorization();
-		training_data = RatingData.Read(args[0]);
-		test_data = RatingData.Read(args[1]);
-		update_data = new RatingsProxy(test_data, update_indices);
-		eval_data   = new RatingsProxy(test_data, eval_indices);
-		foldin_recommender.Ratings = training_data;
-		Console.Write("normal training ... ");
-		foldin_recommender.Train();
-		Console.WriteLine("done.");
-		// IIIb. fold-in evaluate
-		Console.Write("fold-in: ");
-		Console.WriteLine(foldin_recommender.EvaluateFoldIn(update_data, eval_data));
+		Console.WriteLine("fold-in: {0}", recommender.EvaluateFoldIn(update_data, eval_data));
 	}
 }
