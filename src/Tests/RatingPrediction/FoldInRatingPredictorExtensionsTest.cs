0bdfa7f7e89e1ab556a8dcb508e8d187dcede9b3
Zeno Gantner <zeno.gantner@gmail.com>
2012-02-01 14:26:02 +0100

    add extension methods for fold-in (rating predictions), including unit tests

---
+++ src/Tests/RatingPrediction/FoldInRatingPredictorExtensionsTest.cs
@@ -0,0 +1,82 @@
+// Copyright (C) 2012 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using MyMediaLite.DataType;
+using MyMediaLite.IO;
+using MyMediaLite.RatingPrediction;
+
+namespace Tests.RatingPrediction
+{
+	[TestFixture()]
+	public class FoldInRatingPredictorExtensionsTest
+	{
+		IFoldInRatingPredictor CreateRecommender()
+		{
+			var training_data = RatingData.Read("../../../../data/ml-100k/u.data");
+			var recommender = new MatrixFactorization();
+			recommender.Ratings = training_data;
+			recommender.NumFactors = 4;
+			recommender.NumIter = 5;
+			recommender.Train();
+
+			return recommender;
+		}
+
+		[Test()]
+		public void TestTopNWithCandidates()
+		{
+			var rated_items = new List<Pair<int, float>>();
+			rated_items.Add(new Pair<int, float>(1, 1.0f));
+			rated_items.Add(new Pair<int, float>(2, 4.0f));
+			rated_items.Add(new Pair<int, float>(3, 4.5f));
+
+			var candidate_items = new int[] { 4, 5, 6, 7, 8 };
+
+			IFoldInRatingPredictor recommender = CreateRecommender();
+
+			var result = recommender.RecommendItems(rated_items, candidate_items, 3);
+			Assert.GreaterOrEqual(result[0].Second, result[1].Second);
+			Assert.GreaterOrEqual(result[0].Second, result[2].Second);
+			Assert.GreaterOrEqual(result[1].Second, result[2].Second);
+			Assert.Contains(result[0].First, candidate_items);
+			Assert.Contains(result[1].First, candidate_items);
+			Assert.Contains(result[2].First, candidate_items);
+			Assert.AreEqual(3, result.Count);
+		}
+
+		[Test()]
+		public void TestTopNWithoutCandidates()
+		{
+			var rated_items = new List<Pair<int, float>>();
+			rated_items.Add(new Pair<int, float>(1, 1.0f));
+			rated_items.Add(new Pair<int, float>(2, 4.0f));
+			rated_items.Add(new Pair<int, float>(3, 4.5f));
+
+			IFoldInRatingPredictor recommender = CreateRecommender();
+
+			var result = recommender.RecommendItems(rated_items, 3);
+			Assert.GreaterOrEqual(result[0].Second, result[1].Second);
+			Assert.GreaterOrEqual(result[0].Second, result[2].Second);
+			Assert.GreaterOrEqual(result[1].Second, result[2].Second);
+			Assert.AreEqual(3, result.Count);
+		}
+	}
+}
+
