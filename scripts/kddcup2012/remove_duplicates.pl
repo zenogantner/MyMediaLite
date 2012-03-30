f2eff93db269bceb7bc607cff23f45934abdba8e
Zeno Gantner <zeno.gantner@gmail.com>
2012-03-30 13:04:02 +0200

    add remove_duplicates.pl

---
+++ scripts/kddcup2012/remove_duplicates.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+# Remove duplicates from validation dataset
+#
+# Script for KDD Cup 2012, track 1
+# author: Zeno Gantner <zeno.gantner@gmail.com>
+# This script is in the public domain.
+#
+
+use strict;
+use warnings;
+
+use English qw( -no_match_vars );
+use Getopt::Long;
+use POSIX qw(strftime);
+
+my $separator_regex = qr{\t};
+
+my $event_count = 0;
+my %result    = ();
+my %timestamp = ();
+
+LINE:
+while (<>) {
+    my $line = $_;
+    chomp $line;
+
+    my ($user, $item, $result, $timestamp) = split $separator_regex, $line;
+
+    $event_count++;
+
+    my $key = "$user $item";
+    if (exists $result{$key} && $result != 1) {
+        if ($result{$key} == $result) {
+            $timestamp{$key} = $timestamp;
+        }
+    }
+    else {
+        $result{$key}    = $result;
+        $timestamp{$key} = $timestamp;
+    }
+}
+
+print STDERR "Sorting and printint to STDOUT ...\n";
+foreach my $key (sort { $timestamp{$a} <=> $timestamp{$b} } keys %result) {
+    print "$key $result{$key} $timestamp{$key}\n";
+}
+print STDERR "Done.\n";
