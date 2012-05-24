a2838ea39427a6ed123f7e060ad7d10bd7ddb968
Zeno Gantner <zeno.gantner@gmail.com>
2012-05-24 22:57:17 +0200

    simplify MSD Challenge conversion script

--- scripts/msdchallenge/create_submission.sh
+++ scripts/msdchallenge/create_submission.sh
@@ -3,6 +3,5 @@
 # create submission for the Million Song Dataset Challenge:
 # http://www.kaggle.com/c/msdchallenge/
 
-cat kaggle_users.txt | perl -ne 'chomp; print "$_\t" . ++$l . "\n"' | sort -k 1b,1 > user_order.txt
-perl -ne '/(.*?)\t\[(.*)\]/; $u=$1; $s=$2; $s=~s/:.+?,/ /g; $s=~s/:.+//; print "$u\t$s\n"' | sort -k 1b,1 | join --check-order -t '	' -o '2.2 1.2' - user_order.txt | sort -g | cut -f 2
-rm user_order.txt
+sort -g | perl -ne '/(.*?)\t\[(.*)\]/; $u=$1; $s=$2; $s=~s/:.+?,/ /g; $s=~s/:.+//; print "$u\t$s\n"' | cut -f 2
+
