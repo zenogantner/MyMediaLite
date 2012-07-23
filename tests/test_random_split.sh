8f61f3f107df1a7c5ab7cb1cbeaf3fce04bd7137
Zeno Gantner <zeno.gantner@gmail.com>
2012-07-23 09:42:03 +0200

    fix test_random_split.sh

--- tests/test_random_split.sh
+++ tests/test_random_split.sh
@@ -49,8 +49,8 @@ echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=MostPopular 
 echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=WRMF --data-dir=$DATA_DIR --random-seed=1 --num-test-users=100 > log2
      $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=WRMF --data-dir=$DATA_DIR --random-seed=1 --num-test-users=100 > log2
 
-grep "100 users" log1 > log1.grep
-grep "100 users" log2 > log2.grep
+grep "test data" log1 > log1.grep
+grep "test data" log2 > log2.grep
 
 diff log1.grep log2.grep
 
