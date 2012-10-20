213862cbb6e59b0fa6be07662fdd605e6cb545d5
Zeno Gantner <zeno.gantner@gmail.com>
2012-10-21 00:03:05 +0200

    add some more unit tests

---
+++ src/Tests/ItemRecommendation/ItemRecommendersTest.cs
@@ -0,0 +1,39 @@
+// Copyright (C) 2012 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+using NUnit.Framework;
+using MyMediaLite;
+
+namespace Tests.ItemRecommendation
+{
+	[TestFixture()]
+	public class ItemRecommendersTest
+	{
+		[Test()]
+		public void TestToString()
+		{
+			foreach (Type type in Utils.GetTypes("MyMediaLite.ItemRecommendation"))
+				if (!type.IsAbstract && !type.IsInterface && !type.IsEnum && !type.IsGenericType && type.GetInterface("IRatingPrediction") != null)
+				{
+					var recommender = type.CreateRatingPredictor();
+					Assert.IsFalse(recommender.ToString().Contains(","));
+				}
+		}
+	}
+}
+
