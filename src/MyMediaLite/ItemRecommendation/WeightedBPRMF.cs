51280605d5cd5c1c7b669bf8453f67b8b912918e
Zeno Gantner <zeno.gantner@gmail.com>
2012-11-11 20:15:52 +0100

    WeightedBPRMF: fix sampling corner case; set defaults already in constructor

--- src/MyMediaLite/ItemRecommendation/WeightedBPRMF.cs
+++ src/MyMediaLite/ItemRecommendation/WeightedBPRMF.cs
@@ -32,7 +32,13 @@ namespace MyMediaLite.ItemRecommendation
 	public class WeightedBPRMF : BPRMF
 	{
 		/// <summary>Default constructor</summary>
-		public WeightedBPRMF() { }
+		public WeightedBPRMF()
+		{
+			// de-activate until supported
+			WithReplacement = false;
+			// de-activate until false is supported
+			UniformUserSampling = true;
+		}
 
 		///
 		public override void Train()
@@ -49,13 +55,13 @@ namespace MyMediaLite.ItemRecommendation
 		protected override void SampleTriple(out int u, out int i, out int j)
 		{
 			// sample user from positive user-item pairs
-			int index = random.Next(Feedback.Count - 1);
+			int index = random.Next(Feedback.Count);
 			u = Feedback.Users[index];
 			i = Feedback.Items[index];
 
 			// sample negative item
 			do
-				j = Feedback.Items[random.Next(Feedback.Count - 1)];
+				j = Feedback.Items[random.Next(Feedback.Count)];
 			while (Feedback.UserMatrix[u, j]);
 		}
 
