0ca202d93ff1c259a36d9e04b2ed75c8424e6d37
Zeno Gantner <zeno.gantner@gmail.com>
2012-11-19 08:18:30 +0100

    SoftMarginRankingMF: show all relevant options in ToString()

--- src/MyMediaLite/ItemRecommendation/SoftMarginRankingMF.cs
+++ src/MyMediaLite/ItemRecommendation/SoftMarginRankingMF.cs
@@ -121,8 +121,8 @@ namespace MyMediaLite.ItemRecommendation
 		{
 			return string.Format(
 				CultureInfo.InvariantCulture,
-				"{0} num_factors={1} bias_reg={2} reg_u={3} reg_i={4} reg_j={5} num_iter={6} learn_rate={7}",
-				this.GetType().Name, num_factors, BiasReg, reg_u, reg_i, reg_j, NumIter, learn_rate);
+				"{0} num_factors={1} bias_reg={2} reg_u={3} reg_i={4} reg_j={5} num_iter={6} learn_rate={7} uniform_user_sampling={8} with_replacement={9} update_j={10}",
+				this.GetType().Name, num_factors, BiasReg, reg_u, reg_i, reg_j, NumIter, learn_rate, UniformUserSampling, WithReplacement, UpdateJ);
 		}
 	}
 }
