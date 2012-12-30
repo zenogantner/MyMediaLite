5275cbf5df2e944f524db277289546532cea1bf5
Zeno Gantner <zeno.gantner@gmail.com>
2012-12-30 09:01:03 +0100

    update eval unit tests, add some more

--- src/Tests/Eval/Measures/PrecisionAndRecallTest.cs
+++ src/Tests/Eval/Measures/PrecisionAndRecallTest.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2011 Zeno Gantner
+// Copyright (C) 2011, 2012 Zeno Gantner
 //
 // This file is part of MyMediaLite.
 //
@@ -15,7 +15,6 @@
 //  You should have received a copy of the GNU General Public License
 //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
 //
-
 using System;
 using NUnit.Framework;
 using MyMediaLite.Eval.Measures;
@@ -43,71 +42,39 @@ namespace Tests.Eval.Measures
 			Assert.AreEqual(0, PrecisionAndRecall.AP(list3, list_last));
 			Assert.AreEqual((double) 1/5, PrecisionAndRecall.AP(list5, list_last));
 
-			Assert.AreEqual(1, PrecisionAndRecall.AP(list5, list1, list_empty));
-			Assert.AreEqual(1, PrecisionAndRecall.AP(list5, list5, list_empty));
-			Assert.AreEqual(0, PrecisionAndRecall.AP(list3, list_last, list_empty));
-			Assert.AreEqual((double) 1/5, PrecisionAndRecall.AP(list5, list_last, list_empty));
-
-			Assert.AreEqual(0, PrecisionAndRecall.AP(list5, list1, list1)); // special case: empty list
-			Assert.AreEqual(1, PrecisionAndRecall.AP(list5, list5, list1));
-			Assert.AreEqual(0, PrecisionAndRecall.AP(list3, list_last, list1));
-			Assert.AreEqual((double) 1/4, PrecisionAndRecall.AP(list5, list_last, list1));
+			Assert.AreEqual(1, PrecisionAndRecall.AP(list5, list1));
+			Assert.AreEqual(1, PrecisionAndRecall.AP(list5, list5));
+			Assert.AreEqual(0, PrecisionAndRecall.AP(list3, list_last));
+			Assert.AreEqual((double) 1/5, PrecisionAndRecall.AP(list5, list_last));
 		}
 
 		[Test()]
 		public void TestHitsAt()
 		{
-			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list1, list1, list_empty, 1));
-			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, list_empty, 1));
-			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, list_empty, 2));
-			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, list_empty, 3));
-			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, list_empty, 4));
-
-			Assert.AreEqual(0, PrecisionAndRecall.HitsAt(list5, list_last, list3, 1));
-			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list5, list_last, list3, 2));
-			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list5, list_last, list3, 3));
+			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list1, list1, 1));
+			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, 1));
+			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, 2));
+			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, 3));
+			Assert.AreEqual(1, PrecisionAndRecall.HitsAt(list3, list1, 4));
 		}
 
 		[Test()]
 		public void TestPrecisionAt()
 		{
-			Assert.AreEqual(1, PrecisionAndRecall.PrecisionAt(list1, list1, list_empty, 1));
-			Assert.AreEqual(1, PrecisionAndRecall.PrecisionAt(list3, list1, list_empty, 1));
-			Assert.AreEqual((double) 1/2, PrecisionAndRecall.PrecisionAt(list3, list1, list_empty, 2));
-			Assert.AreEqual((double) 1/3, PrecisionAndRecall.PrecisionAt(list3, list1, list_empty, 3));
-
-			Assert.AreEqual(0, PrecisionAndRecall.PrecisionAt(list5, list_last, list3, 1));
-			Assert.AreEqual((double) 1/2, PrecisionAndRecall.PrecisionAt(list5, list_last, list3, 2));
+			Assert.AreEqual(1, PrecisionAndRecall.PrecisionAt(list1, list1, 1));
+			Assert.AreEqual(1, PrecisionAndRecall.PrecisionAt(list3, list1, 1));
+			Assert.AreEqual((double) 1/2, PrecisionAndRecall.PrecisionAt(list3, list1, 2));
+			Assert.AreEqual((double) 1/3, PrecisionAndRecall.PrecisionAt(list3, list1, 3));
 		}
-
-		/*
-		[Test()]
-		public void TestPrecisionAtShorterResultList()
-		{
-			Assert.AreEqual((double) 1/3, PrecisionAndRecall.PrecisionAt(list3, list1, list_empty, 4)); // result list has only length 3
-		}
-		*/
 
 		[Test()]
 		public void TestRecallAt()
 		{
-			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list1, list1, list_empty, 1));
-			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list3, list1, list_empty, 1));
-			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list3, list1, list_empty, 2));
-			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list3, list1, list_empty, 3));
-
-			Assert.AreEqual(0, PrecisionAndRecall.RecallAt(list5, list_last, list3, 1));
-			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list5, list_last, list3, 2));
+			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list1, list1, 1));
+			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list3, list1, 1));
+			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list3, list1, 2));
+			Assert.AreEqual(1, PrecisionAndRecall.RecallAt(list3, list1, 3));
 		}
-
-		/*
-		 * TODO find out how to skip, open ticket for that
-		[Test()]
-		public void TestRecallAtShorterResultList()
-		{
-			Assert.AreEqual((double) 1/3, PrecisionAndRecall.RecallAt(list3, list1, list_empty, 4)); // result list has only length 3
-		}
-		*/
 	}
 }
 
