c7305518deb122d7d2da3dce79b5839fd76be704
Zeno Gantner <zeno.gantner@gmail.com>
2012-12-30 09:11:55 +0100

    ExternalRatingPredictor: rm more imports

--- src/MyMediaLite/RatingPrediction/ExternalRatingPredictor.cs
+++ src/MyMediaLite/RatingPrediction/ExternalRatingPredictor.cs
@@ -14,7 +14,6 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
-using System.IO;
 using MyMediaLite.Data;
 using MyMediaLite.IO;
 
