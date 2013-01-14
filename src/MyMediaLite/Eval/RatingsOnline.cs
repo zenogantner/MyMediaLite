14f87cc35863a6e9fc99c0572c3506238406be79
Zeno Gantner <zeno.gantner@gmail.com>
2013-01-14 18:22:03 +0100

    RatingsOnline: rm unnecessary empty line

--- src/MyMediaLite/Eval/RatingsOnline.cs
+++ src/MyMediaLite/Eval/RatingsOnline.cs
@@ -15,7 +15,6 @@
 //  You should have received a copy of the GNU General Public License
 //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
 //
-
 using System;
 using System.Collections.Generic;
 using MyMediaLite.Data;
