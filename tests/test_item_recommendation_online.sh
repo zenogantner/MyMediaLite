273196ab5fbb40722fd2f5b8e58a799835a95249
Zeno Gantner <zeno.gantner@gmail.com>
2013-01-15 00:19:23 +0100

    add KNN to test_item_recommendation_online.sh

--- tests/test_item_recommendation_online.sh
+++ tests/test_item_recommendation_online.sh
@@ -9,10 +9,19 @@ echo "MovieLens 100K"
 echo "--------------"
 
 DATA_DIR=data/ml-100k
+TEST_USERS=150
 
 for method in MostPopular BPRMF
 do
-	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR --online-evaluation --num-test-users=50 --random-seed=1
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR --online-evaluation --num-test-users=50 --random-seed=1
+	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR --online-evaluation --num-test-users=${TEST_USERS} --random-seed=1
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR --online-evaluation --num-test-users=${TEST_USERS} --random-seed=1
 done
 
+for method in UserKNN ItemKNN
+do
+	for corr in Cosine Cooccurrence
+	do
+		echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR --online-evaluation --num-test-users=${TEST_USERS} --random-seed=1 --recommender-options="correlation=$corr"
+		     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --data-dir=$DATA_DIR --online-evaluation --num-test-users=${TEST_USERS} --random-seed=1 --recommender-options="correlation=$corr"
+	done
+done
