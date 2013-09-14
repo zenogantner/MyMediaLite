1392265dd658455af381b8c126742c9777e60a65
Zeno Gantner <zeno.gantner@gmail.com>
2013-09-14 17:37:05 +0200

    fix test_random_split.sh

--- tests/test_random_split.sh
+++ tests/test_random_split.sh
@@ -11,18 +11,18 @@ echo
 echo "rating predictors"
 echo "-----------------"
 
-echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=GlobalAverage --data-dir=$DATA_DIR --random-seed=1 --prediction-file=pred1
-     $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=GlobalAverage --data-dir=$DATA_DIR --random-seed=1 --prediction-file=pred1
+echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=GlobalAverage --data-dir=$DATA_DIR --random-seed=1 > log1
+     $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=GlobalAverage --data-dir=$DATA_DIR --random-seed=1 > log1
 
-echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=MatrixFactorization --data-dir=$DATA_DIR --random-seed=1 --prediction-file=pred2
-     $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=MatrixFactorization --data-dir=$DATA_DIR --random-seed=1 --prediction-file=pred2
+echo $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=MatrixFactorization --data-dir=$DATA_DIR --random-seed=1 > log2
+     $PROGRAM --training-file=u.data --test-ratio=0.1 --recommender=MatrixFactorization --data-dir=$DATA_DIR --random-seed=1 > log2
 
-cut -f 1,2 pred1 > pred1.cut
-cut -f 1,2 pred2 > pred2.cut
+grep "test data" log1 > log1.grep
+grep "test data" log2 > log2.grep
 
-diff pred1.cut pred2.cut
+diff log1.grep log2.grep
 
-rm pred1 pred2 pred1.cut pred2.cut
+rm log1 log2 log1.grep log2.grep
 
 echo
 echo "item recommenders"
