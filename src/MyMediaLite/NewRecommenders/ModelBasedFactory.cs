885afc0f1e6d39a9101d6228b3355375d0768ff8
Zeno Gantner <zeno.gantner@gmail.com>
2013-10-07 09:59:01 +0200

    add ModelBasedFactory

---
+++ src/MyMediaLite/NewRecommenders/ModelBasedFactory.cs
@@ -0,0 +1,33 @@
+// Copyright (C) 2013 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+
+namespace MyMediaLite
+{
+	abstract public class ModelBasedFactory : IFactory
+	{
+		abstract public IModel LoadModel(string filename);
+		abstract public INewRecommender CreateRecommender(IModel model);
+		public INewRecommender CreateRecommender(IModel model, IDataSet dataset)
+		{
+			return CreateRecommender(model);
+		}
+		abstract public ITrainer CreateTrainer();
+	}
+}
+
