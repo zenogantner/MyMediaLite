0620fb3b00e97039fb4cc8c6bf1f8be3271309a8
Zeno Gantner <zeno.gantner@gmail.com>
2013-10-07 09:17:36 +0200

    attempt at coming up with a new API for recommenders: initial interface definition

---
+++ src/MyMediaLite/NewAPI/ITrainer.cs
@@ -0,0 +1,30 @@
+// Copyright (C) 2013 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+using System.Collections.Generic;
+
+namespace MyMediaLite
+{
+	public interface ITrainer
+	{
+		bool SupportsUpdate { get; }
+		IModel Train(IDataSet dataset, Dictionary<string, object> parameters);
+		IModel Update(IModel model, IDataSet dataset, IList<int> modifiedUsers, IList<int> modifiedItems, Dictionary<string, object> parameters);
+	}
+}
+
