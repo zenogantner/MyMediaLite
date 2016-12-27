5a4fb090d71416c560beed8468c7df7dd6b849af
Zeno Gantner <zeno.gantner@gmail.com>
2016-12-27 21:33:25 +0100

    more robust download script

--- scripts/download_ml-100k.sh
+++ scripts/download_ml-100k.sh
@@ -8,7 +8,7 @@ echo "Refer to the README files for the details of the usage license."
 cd data
 
 wget http://grouplens.org/system/files/ml-100k.zip
-unzip ml-100k.zip
+unzip -o ml-100k.zip
 rm ml-100k.zip
 ../scripts/ml100k_genres.pl ml-100k/u.item > ml-100k/item-attributes-genres.txt
 
