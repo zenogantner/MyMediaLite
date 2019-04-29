d7448d8934d4456bf03c2d4df8ff851dca02e669
Zeno Gantner <zeno.gantner@gmail.com>
2019-04-29 17:24:13 +0200

    more verbose test output

--- tests/test_load_save.sh
+++ tests/test_load_save.sh
@@ -14,9 +14,9 @@ echo "-----------------"
 for method in MatrixFactorization BiasedMatrixFactorization UserItemBaseline GlobalAverage UserAverage
 do
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time\s*\S+//g" > output1.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time\s*\S+//g" | tee output1.txt
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time\s*\S+//g" > output2.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time\s*\S+//g" | tee output2.txt
 	diff --ignore-space-change output1.txt output2.txt
 	rm tmp.model*
 done
@@ -24,9 +24,9 @@ done
 
 method=ItemAverage
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt
-     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt | perl -pe "s/\w+_time\s*\S+//g" > output1.txt
+     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt | perl -pe "s/\w+_time\s*\S+//g" | tee output1.txt
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt
-     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt | perl -pe "s/\w+_time\s*\S+//g" > output2.txt
+     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt | perl -pe "s/\w+_time\s*\S+//g" | tee output2.txt
 diff --ignore-space-change output1.txt output2.txt
 rm tmp.model* um.txt im.txt
 
@@ -34,9 +34,9 @@ rm tmp.model* um.txt im.txt
 for method in SVDPlusPlus SigmoidSVDPlusPlus SigmoidUserAsymmetricFactorModel SigmoidItemAsymmetricFactorModel SigmoidCombinedAsymmetricFactorModel
 do
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt --recommender-options=\"num_iter=1\"
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt --recommender-options="num_iter=1" | perl -pe "s/\w+_time\s*\S+//g" | perl -ne '$ln = $_; while (/(\d+\.\d+)/g) { $f = sprintf "%.4", $1; $ln =~ s/$1/$f/; } print $ln' > output1.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt --recommender-options="num_iter=1" | perl -pe "s/\w+_time\s*\S+//g" | perl -ne '$ln = $_; while (/(\d+\.\d+)/g) { $f = sprintf "%.4", $1; $ln =~ s/$1/$f/; } print $ln' | tee output1.txt
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt --recommender-options=\"num_iter=1\"
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt --recommender-options="num_iter=1" | perl -pe "s/\w+_time\s*\S+//g" | perl -ne '$ln = $_; while (/(\d+\.\d+)/g) { $f = sprintf "%.4", $1; $ln =~ s/$1/$f/; } print $ln' > output2.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt --recommender-options="num_iter=1" | perl -pe "s/\w+_time\s*\S+//g" | perl -ne '$ln = $_; while (/(\d+\.\d+)/g) { $f = sprintf "%.4", $1; $ln =~ s/$1/$f/; } print $ln' | tee output2.txt
 	diff --ignore-space-change output1.txt output2.txt
 	rm tmp.model* um.txt im.txt
 done
@@ -47,9 +47,9 @@ do
 	for c in BinaryCosine Pearson ConditionalProbability
 	do
 		echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$c" --save-model=tmp.model --data-dir=$DATA_DIR
-		     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$c" --save-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time \S+//g" > output1.txt
+		     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$c" --save-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time \S+//g" | tee output1.txt
 		echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$c" --load-model=tmp.model --data-dir=$DATA_DIR
-		     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$c" --load-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time \S+//g" > output2.txt
+		     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$c" --load-model=tmp.model --data-dir=$DATA_DIR | perl -pe "s/\w+_time \S+//g" | tee output2.txt
 		diff --ignore-space-change output1.txt output2.txt
 		rm tmp.model*
 	done
@@ -57,9 +57,9 @@ done
 
 method=ItemAttributeKNN
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --save-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt
-     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --save-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt | perl -pe "s/\w+_time \S+//g" > output1.txt
+     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --save-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt | perl -pe "s/\w+_time \S+//g" | tee output1.txt
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --load-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt
-     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --load-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt | perl -pe "s/\w+_time \S+//g" > output2.txt
+     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --load-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt | perl -pe "s/\w+_time \S+//g" | tee output2.txt
 diff --ignore-space-change output1.txt output2.txt
 rm tmp.model*
 
@@ -72,9 +72,9 @@ PROGRAM="bin/item_recommendation"
 
 method=MostPopular
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --random-seed=1
-     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output1.txt
+     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output1.txt
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --random-seed=1
-     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output2.txt
+     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output2.txt
 diff --ignore-all-space output1.txt output2.txt
 rm tmp.model*
 
@@ -82,9 +82,9 @@ rm tmp.model*
 for method in WRMF BPRMF
 do
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt --recommender-options=\"num_iter=1\" --random-seed=1
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt --recommender-options="num_iter=1" --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output1.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --save-model=tmp.model --data-dir=$DATA_DIR --save-user-mapping=um.txt --save-item-mapping=im.txt --recommender-options="num_iter=1" --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output1.txt
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt --recommender-options=\"num_iter=1\"  --random-seed=1
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt --recommender-options="num_iter=1" --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output2.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --load-model=tmp.model --data-dir=$DATA_DIR --load-user-mapping=um.txt --load-item-mapping=im.txt --recommender-options="num_iter=1" --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output2.txt
 	diff --ignore-all-space output1.txt output2.txt
 	rm tmp.model*
 	rm um.txt im.txt
@@ -95,9 +95,9 @@ do
 	for method in UserKNN ItemKNN
 	do
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$cor" --save-model=tmp.model --data-dir=$DATA_DIR --measures=prec@5 --random-seed=1
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$cor" --save-model=tmp.model --data-dir=$DATA_DIR --measures=prec@5 --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output1.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$cor" --save-model=tmp.model --data-dir=$DATA_DIR --measures=prec@5 --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output1.txt
 	echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$cor" --load-model=tmp.model --data-dir=$DATA_DIR --measures=prec@5 --random-seed=1
-	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$cor" --load-model=tmp.model --data-dir=$DATA_DIR --measures=prec@5 --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output2.txt
+	     $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K correlation=$cor" --load-model=tmp.model --data-dir=$DATA_DIR --measures=prec@5 --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output2.txt
 	diff --ignore-all-space output1.txt output2.txt
 	rm tmp.model*
 	done
@@ -105,9 +105,9 @@ done
 
 method=ItemAttributeKNN
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --save-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt --random-seed=1
-$PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --save-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output1.txt
+$PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --save-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output1.txt
 echo $PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --load-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt --random-seed=1
-$PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --load-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt --random-seed=1 | perl -pe "s/\w+_time \S+//g" > output2.txt
+$PROGRAM --training-file=u1.base --test-file=u1.test --recommender=$method --recommender-options="k=$K" --load-model=tmp.model --data-dir=$DATA_DIR --item-attributes=item-attributes-genres.txt --random-seed=1 | perl -pe "s/\w+_time \S+//g" | tee output2.txt
 diff --ignore-all-space output1.txt output2.txt
 rm tmp.model*
 
